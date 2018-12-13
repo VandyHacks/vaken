651167cf927978d8f73675738ea9e8db149fc0e2
cktang88 <arctangent20@yahoo.com>
2018-12-13 00:25:45 -0600

    Stub hash password pre-hook

--- src/models/user.ts
+++ src/models/user.ts
@@ -1,10 +1,28 @@
 import { Typegoose, prop } from 'typegoose';
+/* import bcrypt from 'bcrypt'; */
 
 const STR_DEFAULT = String();
+
+/**
+ * Typegoose's equivalent to Mongoose pre-hook: On user save password, hash password
+ */
+/*
+@pre<User>('save', next : Function => {
+  bcrypt.hash(this.password, 10, function (err: Error, hash: string){
+    if (err) {
+      return next(err);
+    }
+    this.password = hash;
+    next();
+  })
+})*/
 
 class User extends Typegoose {
   @prop()
   name: string = STR_DEFAULT;
+
+  @prop()
+  password: string = STR_DEFAULT;
 
   @prop()
   email: string = STR_DEFAULT;
@@ -22,4 +40,5 @@ class User extends Typegoose {
   majors: [string] = [STR_DEFAULT];
 }
 
+// const UserModel = new User().getModelForClass(User);
 export default User;
