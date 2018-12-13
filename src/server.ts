cafb4272bffab017b2f1ff379207577135fbe728
cktang88 <arctangent20@yahoo.com>
2018-12-12 21:46:27 -0600

    Add cors + helmet

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,27 @@
 import express from 'express';
+
+import helmet from 'helmet'; // good default security config
+import cors from 'cors';
 import router from './routes/router';
+
+// todo: put in config file
+const whitelist = ['vandyhacks.org'];
+const corsOptions = {
+  origin(origin: string, callback: Function) {
+    if (whitelist.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+};
+
 // create server
 const app = express();
+app.use(helmet);
+
+// can also put over individual routes, with cors-preflight
+app.use(cors(corsOptions));
 app.use(router);
 
 // todo put in config
