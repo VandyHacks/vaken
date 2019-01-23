b3f2baf78c88d3f9876b073f8b0a76f75de408ef
IrfaanKhalid <irfaan.khalid@vanderbilt.edu>
2019-01-23 12:47:16 -0600

    reformat index.ts

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,5 +1,5 @@
-import koa from "koa";
-import koaRouter from "koa-router";
+import koa from 'koa';
+import koaRouter from 'koa-router';
 
 const app = new koa();
 const router = new koaRouter();
@@ -7,12 +7,12 @@ const router = new koaRouter();
 const port = 8080; // default port to listen
 
 // define a route handler for the default home page
-router.get( "/", (ctx, next) => {
-    ctx.body = "Hello World!";
-} );
+router.get('/', (ctx, next) => {
+	ctx.body = 'Hello World!';
+});
 
 app.use(router.routes());
 
 const server = app.listen(port, () => {
-    console.log( `server started at http://localhost:${ port }` );
-} );
+	console.log(`server started at http://localhost:${port}`);
+});
