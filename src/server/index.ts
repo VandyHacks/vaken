a4d5bc4adbe59f37b4697a436488d3ca72e07a40
IrfaanKhalid <irfaan.khalid@vanderbilt.edu>
2019-01-24 13:38:40 -0600

    Updated index.ts

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,5 +1,6 @@
 import koa from 'koa';
 import koaRouter from 'koa-router';
+import userRouter from './api/UserRouter';
 
 const app = new koa();
 const router = new koaRouter();
@@ -14,6 +15,7 @@ router.get('/', (ctx, next) => {
 
 // Add the defined routes to the application
 app.use(router.routes());
+app.use(userRouter.routes());
 
 // Begin listening on the defined port
 const server = app.listen(port, () => {
