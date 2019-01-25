77abcbaa6057da14b6acb45bb09b9368817df940
Matthew Leon <matthew.leon@vanderbilt.edu>
2019-01-25 11:56:03 -0600

    Clean up prod command and infinite dev worker pool

--- webpack.dev.js
+++ webpack.dev.js
@@ -4,13 +4,30 @@ const FriendlyErrorsPlugin = require('friendly-errors-webpack-plugin');
 const webpack = require('webpack');
 const common = require('./webpack.common.js');
 
-module.exports = merge(common, {
+module.exports = merge.smart(common, {
 	mode: 'development',
 	devtool: 'inline-source-map',
 	devServer: {
 		hot: true, // Enable hot module replacement
 		open: true, // Open browser on 'npm start'
 		quiet: true, // Pretty console output
+	},
+	module: {
+		rules: [
+			{
+				// Include ts, tsx, js, and jsx files.
+				test: /\.(ts|js)x?$/,
+				exclude: [/server/, /node_modules/],
+				use: [
+					{
+						loader: 'thread-loader',
+						options: {
+							poolTimeout: Infinity, // set this to Infinity in watch mode - see https://github.com/webpack-contrib/thread-loader
+						},
+					},
+				],
+			},
+		],
 	},
 	plugins: [
 		new webpack.HotModuleReplacementPlugin(), // Auto-refresh on changes
