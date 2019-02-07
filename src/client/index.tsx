8e5ed6bd1c319210da0ad9af5415fd4b417c6a0a
Matthew Leon <matthew.leon@vanderbilt.edu>
2019-02-07 10:40:19 -0600

    add dashboard and additional font weights

--- src/client/index.tsx
+++ src/client/index.tsx
@@ -4,10 +4,11 @@ import { createGlobalStyle } from 'styled-components';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import reset from 'styled-reset';
 import LoginPage from './routes/login/login';
+import Dashboard from './routes/dashboard/dashboard';
 
 const GlobalStyle = createGlobalStyle`
 	body {
-		@import url('https://fonts.googleapis.com/css?family=Roboto+Condensed:400,500|Roboto:400,500,700');
+		@import url('https://fonts.googleapis.com/css?family=Roboto+Condensed:300,400,500|Roboto:300,400,500,700');
 		font-family: 'Roboto', sans-serif;
 
 		${reset}
@@ -26,6 +27,7 @@ const Vaken = (): JSX.Element => {
 			<BrowserRouter>
 				<Switch>
 					<Route path="/" component={LoginPage} />
+					<Route path="/dashboard" component={Dashboard} />
 				</Switch>
 			</BrowserRouter>
 		</>
