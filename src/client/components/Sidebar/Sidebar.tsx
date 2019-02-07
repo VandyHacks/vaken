f81d1423a7dcde10388c6851d1d52548c29f2cb0
Matthew Leon <matthew.leon@vanderbilt.edu>
2019-02-07 10:39:12 -0600

    add full hackathon title to strings.json

---
+++ src/client/components/Sidebar/Sidebar.tsx
@@ -0,0 +1,97 @@
+import React from 'react';
+import styled from 'styled-components';
+import { NavLink as UglyNavLink, withRouter } from 'react-router-dom';
+import sqLogo from '../../assets/img/square_hackathon_logo.svg';
+import STRINGS from '../../assets/strings.json';
+import NavButton from '../Buttons/NavButton';
+import { FlexStartColumn, FlexEndColumn } from '../Containers/FlexContainers';
+import SmallCenteredText from '../Text/SmallCenteredText';
+
+interface Props {}
+
+const Layout = styled.div`
+	grid-area: sidebar;
+`;
+
+const Background = styled.div`
+	background: linear-gradient(254.59deg, #bd7ae3 0%, #8461c9 100%);
+	width: 100%;
+	height: 100%;
+
+	display: flex;
+	flex-flow: column nowrap;
+	justify-content: flex-start;
+	align-items: flex-start;
+`;
+
+const Logo = styled.img`
+	margin: 3rem 2rem;
+	width: min-content;
+	align-self: center;
+`;
+
+const HorizontalLine = styled.hr`
+	margin: 1rem 2rem;
+	width: calc(100% - 4rem);
+	color: white;
+`;
+
+const NavButtonWhiteText = styled(NavButton)`
+	color: white;
+	font-weight: 100;
+	font-size: 28pt;
+`;
+
+const NavLink = styled(UglyNavLink)`
+	text-decoration: none;
+	text-align: left;
+	height: max-content;
+	width: 100%;
+
+	&:focus,
+	&:hover,
+	&:visited,
+	&:link,
+	&:active {
+		text-decoration: none;
+	}
+`;
+
+const Sidebar = withRouter(
+	(props: Props): JSX.Element => {
+		return (
+			<Layout>
+				<Background>
+					<Logo src={'/' + sqLogo} alt={STRINGS.SQUARE_LOGO_ALT_TEXT} />
+					<HorizontalLine />
+					<FlexStartColumn>
+						<NavLink to="/dashboard">
+							<NavButtonWhiteText text="Dashboard" />
+						</NavLink>
+						<NavLink to="/profile">
+							<NavButtonWhiteText text="Profile" />
+						</NavLink>
+						<NavLink to="/application">
+							<NavButtonWhiteText text="Application" />
+						</NavLink>
+						<NavLink to="/team">
+							<NavButtonWhiteText text="Team" />
+						</NavLink>
+						<NavLink to="/help">
+							<NavButtonWhiteText text="Help" />
+						</NavLink>
+					</FlexStartColumn>
+					<FlexEndColumn>
+						<NavLink to="/logout">
+							<NavButtonWhiteText text="Logout" />
+						</NavLink>
+						<HorizontalLine />
+						<SmallCenteredText>{STRINGS.HACKATHON_TITLE}</SmallCenteredText>
+					</FlexEndColumn>
+				</Background>
+			</Layout>
+		);
+	}
+);
+
+export default Sidebar;
