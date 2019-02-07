d29b0810976308235b92ece2a7733e947f69af73
Matthew Leon <matthew.leon@vanderbilt.edu>
2019-02-07 14:02:44 -0600

    factor stateless functions out into helper files

--- src/client/routes/login/passwordLogin.tsx
+++ src/client/routes/login/passwordLogin.tsx
@@ -8,6 +8,8 @@ import { FlexRow, SpaceBetweenColumn } from '../../components/Containers/FlexCon
 import TextLink from '../../components/Text/TextLink';
 import STRINGS from '../../assets/strings.json';
 import LeftImgTextInput from '../../components/Input/LeftImgTextInput';
+import { emailValidation, passwordValidation } from '../../../common/ValidationFunctions';
+import { onChangeWrapper, checkValid } from '../../components/Input/helperFunctions';
 
 interface Props {}
 interface State {
@@ -15,56 +17,6 @@ interface State {
 	emailValid: boolean; // Validation state for email field. Used for red underline and sending
 	pass: string;
 	passValid: boolean; // Validation state for password field. Used for red underline and sending
-}
-
-/**
- * onChangeWrapper wraps a setState function to take a react Input event function
- * @param {function} updateFn - function that will update the state
- * @returns {function} function suitable for a react input onChange={} prop
- */
-export function onChangeWrapper(
-	updateFn: (p: string) => void
-): (e: React.ChangeEvent<HTMLInputElement>) => void {
-	return (e: React.ChangeEvent<HTMLInputElement>) => {
-		const { value } = e.target;
-		updateFn(value);
-	};
-}
-
-/**
- * emailValidation validates that the input e is a valid email address
- * @param {string} e - input to validate as email address
- * @returns {boolean} true if e is an email, otherwise false
- */
-function emailValidation(e: string): boolean {
-	// Validate email using regex
-	return /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(e);
-}
-
-/**
- * emailValidation validates that the input p contains at least 1 symbol (@$!%*#?&),
- * 1 number, 1 letter, and is at least 8 characters long
- * @param {string} p - input to validate as password
- * @returns {boolean} true if p contains a lowercase letter, uppercase letter, , otherwise false
- */
-function passwordValidation(p: string): boolean {
-	// Validate password of length > 8, number, letter, and symbol using regex
-	return /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/.test(p);
-}
-
-/**
- * checkValid is a factory method for a validation function
- * @param {T} input - the input on which to run the validation fn
- * @param {function} validFn - function to validate input
- * @param {function} updateFn - function to asynchronously update the state of the valid function
- * @return {function} an event handler function to run to update the valid status of the input
- */
-export function checkValid<T>(
-	input: T,
-	validFn: (p: T) => boolean,
-	updateFn: (p: boolean) => void
-): () => void {
-	return () => updateFn(validFn(input));
 }
 
 /**
