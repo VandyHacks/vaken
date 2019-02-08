1063de25414fa53d40a3b4c44e1eeaf897e764e1
IrfaanKhalid <irfaan.khalid@vanderbilt.edu>
2019-02-08 11:54:32 -0600

    Sample schema!

--- src/server/data/schema.ts
+++ src/server/data/schema.ts
@@ -1,14 +1,51 @@
 import { makeExecutableSchema, addMockFunctionsToSchema } from 'graphql-tools';
-import mocks from './mocks';
 
 const typeDefs = `
-type Query {
-  testString: String
-}
+  type Author {
+    id: Int!
+    firstName: String
+    lastName: String
+    """
+    the list of Posts by this author
+    """
+    posts: [Post]
+  }
+
+  type Post {
+    id: Int!
+    title: String
+    author: Author
+    votes: Int
+  }
+
+  # the schema allows the following query:
+  type Query {
+    posts: [Post]
+    author(id: Int!): Author
+  }
 `;
 
-const schema = makeExecutableSchema({ typeDefs });
+// example data
+const authors = [
+	{ id: 1, firstName: 'Tom', lastName: 'Coleman' },
+	{ id: 2, firstName: 'Sashko', lastName: 'Stubailo' },
+	{ id: 3, firstName: 'Mikhail', lastName: 'Novikov' },
+];
 
-addMockFunctionsToSchema({ mocks, schema });
+const posts = [
+	{ id: 1, authorId: 1, title: 'Introduction to GraphQL', votes: 2 },
+	{ id: 2, authorId: 2, title: 'Welcome to Meteor', votes: 3 },
+	{ id: 3, authorId: 2, title: 'Advanced GraphQL', votes: 1 },
+	{ id: 4, authorId: 3, title: 'Launchpad is Cool', votes: 7 },
+];
 
-export default schema;
+const resolvers = {
+	Query: {
+		posts: () => posts,
+	},
+};
+
+export const schema = makeExecutableSchema({
+	typeDefs,
+	resolvers,
+});
