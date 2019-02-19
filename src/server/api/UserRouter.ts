fc9c6653ca71cd4dad2210dc07bec76fbab611fe
ftiet <ftiet21@gmail.com>
2019-02-18 19:10:43 -0600

    Local auth, bcrypt setup, and fixed sso issues

--- src/server/api/UserRouter.ts
+++ src/server/api/UserRouter.ts
@@ -13,46 +13,68 @@ userRouter.post('/mongo', async (ctx, next) => {
 	console.log(user);
 });
 
-userRouter.post('/api/login', async ctx => {
+userRouter.post('/api', async ctx => {
 	// Dummy logging for now; TODO - flesh out this functionality
 	console.log(ctx.request);
 	ctx.response.status = 200;
 });
 
+userRouter.post('/api/login', async ctx => {
+	return passport.authenticate('local', (err: any, user: any, info: any, status: any) => {
+		console.log('> Local auth');
+		if (user) {
+			ctx.body = { success: true };
+			ctx.login(user);
+			console.log('> User:');
+			console.log(ctx.state.user);
+			ctx.redirect('/dashboard');
+		} else {
+			ctx.body = { success: false };
+			ctx.throw(401);
+			ctx.redirect('/');
+		}
+	})(ctx);
+});
+
 userRouter.get(
 	'/api/auth/google',
-	passport.authenticate('google', { scope: ['openid', 'profile', 'email'], display: 'popup' }),
-	ctx => {
-		console.log('inside /api/auth/google');
-	}
+	passport.authenticate('google', { scope: ['openid', 'profile', 'email'], display: 'popup' })
 );
 
 userRouter.get('/api/auth/google/callback', async ctx => {
 	return passport.authenticate('google', (err: any, user: any, info: any, status: any) => {
-		console.log('> User:');
-		console.log(ctx.state.user);
-		ctx.redirect('/');
+		if (user) {
+			ctx.body = { success: true };
+			ctx.login(user);
+			console.log('> User:');
+			console.log(ctx.state.user);
+			ctx.redirect('/dashboard');
+		} else {
+			ctx.body = { success: false };
+			ctx.throw(401);
+			ctx.redirect('/');
+		}
 	})(ctx);
 });
 
-userRouter.get(
-	'/api/auth/github',
-	passport.authenticate('github', { scope: ['user:email'] }),
-	ctx => {
-		console.log('inside /api/auth/github');
-	}
-);
+userRouter.get('/api/auth/github', passport.authenticate('github', { scope: ['user:email'] }));
 
 userRouter.get('/api/auth/github/callback', async ctx => {
 	return passport.authenticate('github', (err: any, user: any, info: any, status: any) => {
-		// console.log(ctx, err, user, info, status);
-		console.log('> User:');
-		console.log(ctx.state.user);
-		ctx.redirect('/');
+		if (user) {
+			ctx.body = { success: true };
+			ctx.login(user);
+			console.log('> User:');
+			console.log(ctx.state.user);
+			ctx.redirect('localhost:8081/dashboard');
+		} else {
+			ctx.body = { success: false };
+			ctx.throw(401);
+			ctx.redirect('/');
+		}
 	})(ctx);
 });
 
-// app.use(userRouter.routes()).use(userRouter.allowedMethods());
 export default userRouter;
 
 // Copyright (c) 2019 Vanderbilt University
