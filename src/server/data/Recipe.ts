988e23b5e6ec0e0077848cb7ab6e55a2ea107267
IrfaanKhalid <irfaan.khalid@vanderbilt.edu>
2019-02-21 20:40:28 -0600

    Functioning example of a fully typed and decorated GQL schema being resolved!

---
+++ src/server/data/Recipe.ts
@@ -0,0 +1,40 @@
+import 'reflect-metadata';
+import { Field, ObjectType, Int, Float } from 'type-graphql';
+
+/*
+ * TODO - build a proper constructor to avoid the ! hack
+ * (https://github.com/Microsoft/TypeScript-Vue-Starter/issues/36)
+ */
+
+@ObjectType({ description: 'Object representing cooking recipe' })
+export class Recipe {
+	@Field()
+	title!: string;
+
+	@Field(type => String, { nullable: true, deprecationReason: 'Use `description` field instead' })
+	get specification(): string | undefined {
+		return this.description;
+	}
+
+	@Field({ nullable: true, description: 'The recipe description with preparation info' })
+	description?: string;
+
+	@Field(type => [Int])
+	ratings!: number[];
+
+	@Field()
+	creationDate!: Date;
+
+	@Field(type => Int)
+	ratingsCount!: number;
+
+	@Field(type => Float, { nullable: true })
+	get averageRating(): number | null {
+		const ratingsCount = this.ratings.length;
+		if (ratingsCount === 0) {
+			return null;
+		}
+		const ratingsSum = this.ratings.reduce((a, b) => a + b, 0);
+		return ratingsSum / ratingsCount;
+	}
+}
