6c2d612092fba52402e789de52c98747f51b5ea4
IrfaanKhalid <irfaan.khalid@vanderbilt.edu>
2019-02-21 20:51:48 -0600

    Added fields to User DTO

--- src/server/data/User.ts
+++ src/server/data/User.ts
@@ -1,5 +1,40 @@
 import 'reflect-metadata';
 import { Field, ObjectType, Int, Float } from 'type-graphql';
 
+/**
+ * TODO - build explicit constructor (can I just set default values?)
+ * TODO - enforce uniqueness as necessary
+ * TODO - enforce required vs optional status as necessary
+ */
 @ObjectType({ description: 'DTO for a generic Vaken user' })
-export class User {}
+export class User {
+	@Field(type => [String])
+	nfcCodes!: string[];
+
+	@Field()
+	firstName!: string;
+
+	@Field()
+	lastName!: string;
+
+	@Field()
+	email!: string;
+
+	@Field()
+	google?: string;
+
+	@Field()
+	github?: string;
+
+	@Field()
+	phoneNumber!: string;
+
+	@Field()
+	gender!: string;
+
+	@Field()
+	shirtSize!: string;
+
+	@Field()
+	dietaryRestrictions?: string;
+}
