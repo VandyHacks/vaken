917a566a0a3dffc5bc71c1e5d8296de049726cdc
IrfaanKhalid <irfaan.khalid@vanderbilt.edu>
2019-02-21 21:13:41 -0600

    Add function to mock data

--- src/server/resolvers/UserResolver.ts
+++ src/server/resolvers/UserResolver.ts
@@ -17,28 +17,45 @@ import { User } from '../data/User';
  * TODO - add mutations
  */
 @Resolver(of => User)
-export class UserResolver implements ResolverInterface<User> {
-}
+export class UserResolver implements ResolverInterface<User> {}
 
-// let createUserSamples = () => {
-// 	return plainToClass(User, [
-// 		{
-// 			description: 'Desc 1',
-// 			title: 'User 1',
-// 			ratings: [0, 3, 1],
-// 			creationDate: new Date('2018-04-11'),
-// 		},
-// 		{
-// 			description: 'Desc 2',
-// 			title: 'User 2',
-// 			ratings: [4, 2, 3, 1],
-// 			creationDate: new Date('2018-04-15'),
-// 		},
-// 		{
-// 			description: 'Desc 3',
-// 			title: 'User 3',
-// 			ratings: [5, 4],
-// 			creationDate: new Date(),
-// 		},
-// 	]);
+let createUserSamples = () => {
+	return plainToClass(User, [
+		{
+			nfcCodes: [],
+			firstName: 'Matthew',
+			lastName: 'Leon',
+			email: 'ml@mattleon.com',
+			phoneNumber: '+19876543210',
+			gender: 'male',
+			shirtSize: 'M',
+		},
+		{
+			nfcCodes: [],
+			firstName: 'Irfaan',
+			lastName: 'Khalid',
+			email: 'irfaan.khalid@vanderbilt.edu',
+			phoneNumber: '+18152809862',
+			gender: 'male',
+			shirtSize: 'M',
+		},
+		{
+			nfcCodes: [],
+			firstName: 'Alan',
+			lastName: 'Wilms',
+			email: 'alan.wilms@vanderbilt.edu',
+			phoneNumber: '+19283403498',
+			gender: 'male',
+			shirtSize: 'M',
+		},
+		{
+			nfcCodes: [],
+			firstName: 'Felix',
+			lastName: 'Tiet',
+			email: 'felix.tiet@vanderbilt.edu',
+			phoneNumber: '+18888888888',
+			gender: 'male',
+			shirtSize: 'S',
+		},
+	]);
 };
