5f595859ae57ecfc2fed12592cd4877ebe245236
IrfaanKhalid <irfaan.khalid@vanderbilt.edu>
2019-03-01 12:23:37 -0600

    Added Mentor DTO

---
+++ src/server/data/Mentor.ts
@@ -0,0 +1,13 @@
+import 'reflect-metadata';
+import { Field, ObjectType } from 'type-graphql';
+
+import { User } from './User';
+
+@ObjectType({ description: 'DTO for a Vaken mentor' })
+export class Mentor extends User {
+	@Field(type => [String])
+	shifts!: [String];
+
+	@Field(type => [String])
+	skills!: [String];
+}
