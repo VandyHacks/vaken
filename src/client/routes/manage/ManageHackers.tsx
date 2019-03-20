1c4019734b6a52b28b7d00c02e8a32d85f17cbb7
AlanWilms <alan.p.wilms@gmail.com>
2019-03-20 15:06:54 -0500

    add working GraphQL query and fix Router render error

--- src/client/routes/manage/ManageHackers.tsx
+++ src/client/routes/manage/ManageHackers.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import HackerTable from './HackerTable';
 import hackerData from '../../assets/hackerData.json';
 import FloatingPopup from '../../components/Containers/FloatingPopup';
+import { gql } from 'apollo-boost';
+import { Query } from 'react-apollo';
 
 const ManageHackers = (): JSX.Element => {
 	// GraphQL query would go here
@@ -10,17 +12,36 @@ const ManageHackers = (): JSX.Element => {
 	// 	return value;
 	// });
 
+	const GET_HACKERS = gql`
+		query {
+			getAllHackers {
+				firstName
+				lastName
+				needsReimbursement
+				email
+				school
+				gradYear
+			}
+		}
+	`;
+
 	return (
-		<>
-			<FloatingPopup
-				borderRadius="1rem"
-				height="100%"
-				backgroundOpacity="1"
-				padding="1.5rem"
-				>
-				<HackerTable data={tableData} />
-			</FloatingPopup>
-		</>
+		<Query query={GET_HACKERS}>
+			{({ loading, error, data }: any) => {
+				if (loading) return <div>Loading...</div>;
+				if (error) {
+					console.log(error);
+					return <div>Error :(</div>;
+				}
+				return (
+					<>
+						<FloatingPopup borderRadius="1rem" height="100%" backgroundOpacity="1" padding="1.5rem">
+							<HackerTable data={tableData} />
+						</FloatingPopup>
+					</>
+				);
+			}}
+		</Query>
 	);
 };
 
