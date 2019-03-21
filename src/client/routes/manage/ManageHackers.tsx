6e0008681af46269c2e86dff61d48dc5de255d57
AlanWilms <alan.p.wilms@gmail.com>
2019-03-21 01:09:42 -0500

    create spinner and start of error text

--- src/client/routes/manage/ManageHackers.tsx
+++ src/client/routes/manage/ManageHackers.tsx
@@ -4,6 +4,9 @@ import hackerData from '../../assets/hackerData.json';
 import FloatingPopup from '../../components/Containers/FloatingPopup';
 import { gql } from 'apollo-boost';
 import { Query } from 'react-apollo';
+import { Spinner } from '../../components/Loading/Spinner';
+// @ts-ignore
+import SadFace from '../../assets/img/sad_face.svg?inline';
 
 const ManageHackers = (): JSX.Element => {
 	// GraphQL query would go here
@@ -28,15 +31,10 @@ const ManageHackers = (): JSX.Element => {
 	return (
 		<Query query={GET_HACKERS}>
 			{({ loading, error, data }: any) => {
-				if (loading) return <div>Loading...</div>;
-				if (error) {
-					console.log(error);
-					return <div>Error :(</div>;
-				}
 				return (
 					<>
 						<FloatingPopup borderRadius="1rem" height="100%" backgroundOpacity="1" padding="1.5rem">
-							<HackerTable data={tableData} />
+							{loading ? <Spinner /> : error ? <SadFace /> : <HackerTable data={tableData} />}
 						</FloatingPopup>
 					</>
 				);
