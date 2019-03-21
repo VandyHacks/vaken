604729bc2f174b5fb19d981883a9083171f9b9b4
AlanWilms <alan.p.wilms@gmail.com>
2019-03-21 12:32:30 -0500

    finalize error message and working GraphQL Query to Hacker table

---
+++ src/client/components/Text/ErrorMessage.tsx
@@ -0,0 +1,33 @@
+import React, { FunctionComponent } from 'react';
+import styled from 'styled-components';
+import STRINGS from '../../assets/strings.json';
+// @ts-ignore
+import SadFace from '../../assets/img/sad_face.svg?inline';
+
+const Rectangle = styled.div`
+	border: 0.1rem solid ${STRINGS.WARNING_COLOR};
+	border-radius: 1rem;
+    display: inline-block;
+    text-align: center;
+    color: ${STRINGS.WARNING_COLOR};
+	font-family: 'Roboto', sans-serif;
+	font-weight: 500;
+	font-size: 1.5rem;
+	display: inline-block;
+	padding: 1rem;
+`;
+
+interface Props {
+	children?: JSX.Element;
+}
+
+export const ErrorMessage: FunctionComponent<Props> = (props: Props): JSX.Element => {
+	return (
+		<Rectangle>
+			<SadFace />
+			{props.children}
+		</Rectangle>
+	);
+};
+
+export default ErrorMessage;
