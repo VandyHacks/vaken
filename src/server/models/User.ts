69b6c8c718524a94364322037e3eb548d4faf6aa
ftiet <ftiet21@gmail.com>
2019-03-22 01:23:07 -0500

    Bring back bcrypt

--- src/server/models/User.ts
+++ src/server/models/User.ts
@@ -1,28 +1,25 @@
 import { pre, prop, arrayProp, Typegoose, ModelType, InstanceType } from 'typegoose';
-// import bcrypt from 'bcrypt';
+import bcrypt from 'bcrypt';
 
-// Figure out how we want to handle user roles/auth
-// enum UserRole {}
-
-// const saltRounds = 10;
-// @pre<User>('save', function(next) {
-// 	const user = this;
-// 	if (!user.isModified('password')) {
-// 		return next();
-// 	}
-// 	bcrypt.genSalt(saltRounds, (err, salt) => {
-// 		if (err) {
-// 			return next();
-// 		}
-// 		bcrypt.hash(user.password, salt, (err, hash) => {
-// 			if (err) {
-// 				return next();
-// 			}
-// 			user.password = hash;
-// 			next();
-// 		});
-// 	});
-// })
+const saltRounds = 10;
+@pre<User>('save', function(next) {
+	const user = this;
+	if (!user.isModified('password')) {
+		return next();
+	}
+	bcrypt.genSalt(saltRounds, (err, salt) => {
+		if (err) {
+			return next();
+		}
+		bcrypt.hash(user.password, salt, (err, hash) => {
+			if (err) {
+				return next();
+			}
+			user.password = hash;
+			next();
+		});
+	});
+})
 class User extends Typegoose {
 	@prop({ required: true })
 	email: string = '';
