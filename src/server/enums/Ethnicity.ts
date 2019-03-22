bd47578515e01fe33d826d2bbaa74a260b0c80ea
IrfaanKhalid <irfaan.khalid@vanderbilt.edu>
2019-03-22 12:12:41 -0500

    Fix type issue with Ethnicity enum

--- src/server/enums/Ethnicity.ts
+++ src/server/enums/Ethnicity.ts
@@ -1,8 +1,8 @@
 import { registerEnumType } from 'type-graphql';
 
 export enum Ethnicity {
-	HispanicOrLatino,
-	NotHispanicOrLatino,
+	HispanicOrLatino = 'Hispanic or Latino',
+	NotHispanicOrLatino = 'Not Hispanic or Latino',
 }
 
 registerEnumType(Ethnicity, {
