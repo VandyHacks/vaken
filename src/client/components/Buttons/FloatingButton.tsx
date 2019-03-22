6a0e7ebf8fb24cb3e4ff836e678e327c9c97df78
AlanWilms <alan.p.wilms@gmail.com>
2019-03-22 04:58:32 -0500

    add floating button and group toggle

---
+++ src/client/components/Buttons/FloatingButton.tsx
@@ -0,0 +1,26 @@
+import styled from 'styled-components';
+import STRINGS from '../../assets/strings.json';
+
+export const FloatingButton = styled.button`
+    background-color: ${STRINGS.ACCENT_COLOR};
+    color: #ffffff;
+    font-size: 1rem;
+	position:fixed;
+	width: 8rem;
+	height: 3.75rem;
+	bottom: 3.25rem;
+	right: 3.75rem;
+	border-radius: 3.125rem;
+	text-align:center;
+    &:focus {
+        outline: none;
+        border: none;
+    }
+    &:hover, &:active {
+        box-shadow: 0rem 0rem 0.5rem 0rem #D0C9D6;
+        outline: none;
+        border: none;
+    }
+`;
+
+export default FloatingButton;
