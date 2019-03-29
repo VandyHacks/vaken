ae7541f82ad619cf01a43e82b1bcbae68ee9998a
IrfaanKhalid <irfaan.khalid@vanderbilt.edu>
2019-03-28 21:47:33 -0500

    scaffold logic flow for addHackerToTeam

--- src/server/resolvers/TeamResolver.ts
+++ src/server/resolvers/TeamResolver.ts
@@ -1,4 +1,4 @@
-import { Resolver, Query, Arg } from 'type-graphql';
+import { Resolver, Query, Arg, Mutation } from 'type-graphql';
 import { plainToClass } from 'class-transformer';
 
 import { User } from '../data/User';
@@ -9,6 +9,29 @@ import { teamModel } from '../models/Team';
 
 @Resolver(() => Team)
 class TeamResolver {
+	/**
+	 * @param {string} email - email address of the user to add to a team
+	 * @param {string} teamName - name of the team to which to add the user
+	 * @returns {Status} new status of user or null if the hacker doesn't exist
+	 */
+	@Mutation(() => Team, {
+		description: "Update a Hacker's status and return updated status",
+	})
+	public async addHackerToTeam(
+		@Arg('email', { nullable: false }) email: string,
+		@Arg('teamName') teamName: string
+	) {
+		const team = await teamModel.findOne({ teamName: teamName });
+
+		if (!team) {
+			// Create team, add hacker to it
+		} else if (team.teamMembers.length === teamModel.MAX_SIZE) {
+			// Don't add, return error
+		} else {
+			// Add to team, return success
+		}
+	}
+
 	/**
 	 * @param {string} teamName - email address of a particular user
 	 * @returns {number} Size of the team
