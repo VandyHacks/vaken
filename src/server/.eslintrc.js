5b4aa8e926282db59dbcf394f4cc434a7fe30af3
IrfaanKhalid <irfaan.khalid@vanderbilt.edu>
2019-04-08 12:32:01 -0500

    More updates to config

--- src/server/.eslintrc.js
+++ src/server/.eslintrc.js
@@ -1,9 +1,8 @@
 module.exports = {
 	parser: '@typescript-eslint/parser', // Specifies the ESLint parser
 	extends: [
-		'plugin:@typescript-eslint/recommended', // Uses the recommended rules from the @typescript-eslint/eslint-plugin
-		'plugin:react/recommended',
 		'airbnb', // Airbnb style guide
+		'plugin:@typescript-eslint/recommended', // Uses the recommended rules from the @typescript-eslint/eslint-plugin
 		'prettier/@typescript-eslint', // Uses eslint-config-prettier to disable ESLint rules from @typescript-eslint/eslint-plugin that would conflict with prettier
 		'plugin:prettier/recommended', // Enables eslint-plugin-prettier and displays prettier errors as ESLint errors. Make sure this is always the last configuration in the extends array.
 	],
@@ -16,9 +15,6 @@ module.exports = {
 	},
 	plugins: ['sort-keys-fix', 'react', 'react-hooks'],
 	parserOptions: {
-		ecmaFeatures: {
-			jsx: true,
-		},
 		ecmaVersion: 2018, // Allows for the parsing of modern ECMAScript features
 		sourceType: 'module', // Allows for the use of imports
 		project: './tsconfig.json',
