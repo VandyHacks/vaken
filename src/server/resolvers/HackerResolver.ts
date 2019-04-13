9fc663a39d2aab2b671601787ba9a087f00eb961
IrfaanKhalid <irfaan.khalid@vanderbilt.edu>
2019-04-12 23:40:52 -0500

    Pushing whatever I have fuck them hackers

--- src/server/resolvers/HackerResolver.ts
+++ src/server/resolvers/HackerResolver.ts
@@ -223,23 +223,31 @@ class HackerResolver {
 		@Arg('email', { nullable: false }) email: string,
 		@Arg('teamName') teamName: string
 	): Promise<boolean> {
-		// Make sure the hacker and team exist
+		// Make sure the team and user exist
 		const team = await teamModel.findOne({ teamName });
 		const user = await UserModel.findOne({ authLevel: AuthLevel.HACKER, email });
 
-		// If the hacker doesn't exist, throw an error
+		// If the user doesn't exist, throw an error
 		if (!user) {
 			throw new Error('User does not exist!');
+		}
+
+		// Grab the hacker object associated with the user
+		const hacker = await HackerModel.findOne({ user: user._id });
+		// console.log(hacker.); // debug
+
+		if (!hacker) {
+			throw new Error('Hacker does not exist. The User is probably not a hacker.');
 		}
 
 		// Ensure the team exists and the hacker can join
 		if (!team) {
 			try {
-				await teamModel.create({ size: 1, teamMembers: [{ _id: user._id }], teamName });
+				await teamModel.create({ size: 1, teamMembers: [{ _id: hacker._id }], teamName });
 			} catch (err) {
 				throw new Error('Team could not be created!');
 			}
-		} else if (team.teamMembers.indexOf(user._id) != -1) {
+		} else if (team.teamMembers.indexOf(hacker._id) != -1) {
 			throw new Error('Hacker is already a part of this team!');
 		} else if (team.size === CONSTANTS.MAX_TEAM_SIZE) {
 			throw new Error('This team is already full!');
@@ -248,17 +256,13 @@ class HackerResolver {
 			try {
 				await teamModel.updateOne(
 					{ teamName },
-					{ $push: { teamMembers: { _id: user._id } }, $set: { size: team.size + 1 } }
+					{ $push: { teamMembers: { _id: hacker._id } }, $set: { size: team.size + 1 } }
 				);
+
+				const newName: string = teamName;
+				await HackerModel.updateOne({ user: user._id }, { $set: { teamName: newName } });
 			} catch (err) {
 				throw new Error('Hacker could not be added to team!');
-			}
-
-			// Update the hacker's team
-			try {
-				await HackerModel.updateOne({ _id: user._id }, { $set: { teamName: teamName } });
-			} catch (err) {
-				throw new Error('Hacker team could not be updated!');
 			}
 		}
 
@@ -296,7 +300,7 @@ class HackerResolver {
 
 		// If the team doesn't exist for some ungodly reason, just update the Hacker and throw an error
 		if (!team) {
-			await HackerModel.updateOne({ _id: user._id }, { $set: { teamName: '' } });
+			await HackerModel.updateOne({ user: user._id }, { $set: { teamName: '' } });
 			throw new Error('Team does not exist!');
 		}
 
@@ -309,7 +313,7 @@ class HackerResolver {
 			);
 
 			// Remove teamName from Hacker's profile
-			await HackerModel.updateOne({ _id: user._id }, { $set: { teamName: '' } });
+			await HackerModel.updateOne({ user: user._id }, { $set: { teamName: '' } });
 
 			// If the team is now empty, delete it
 			if (updatedTeam && updatedTeam.size === 0) {
