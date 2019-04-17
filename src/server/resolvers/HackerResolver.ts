10df91dad21e0fc1695908953af16a22ef15137c
AlanWilms <alan.p.wilms@gmail.com>
2019-04-17 17:18:30 -0500

    merge

--- src/server/resolvers/HackerResolver.ts
+++ src/server/resolvers/HackerResolver.ts
@@ -10,15 +10,19 @@ import { HackerModel } from '../models/Hacker';
 import { User, UserModel } from '../models/User';
 import AuthLevel from '../enums/AuthLevel';
 import Status from '../enums/Status';
+import UpdateHackerInput from '../inputs/UpdateHackerInput';
+import { TeamModel } from '../models/Team';
+import CONSTANTS from '../../common/constants.json';
 
 @Resolver(() => Hacker)
 class HackerResolver {
 	/**
 	 * @param {string} email - a Hacker's email address
-	 * @returns {Hacker} a Hacker associated with the provided email address or null if not found
+	 * @returns {Promise<Hacker | null>} a Hacker associated with the provided email address
+	 * 																	 or null if not found
 	 */
 	@Query(() => Hacker, { nullable: true })
-	public static async getHackerByEmail(@Arg('email') email: string): Promise<Hacker | null> {
+	public static async hacker(@Arg('email') email: string): Promise<Hacker | null> {
 		const user = await UserModel.findOne({ authLevel: AuthLevel.HACKER, email });
 		if (!user) {
 			return null;
@@ -44,7 +48,7 @@ class HackerResolver {
 	@Query(() => [Hacker], {
 		description: 'Return all the Hackers in the database',
 	})
-	public static async getAllHackers(): Promise<Hacker[]> {
+	public static async hackers(): Promise<Hacker[]> {
 		const users = await UserModel.find({ authLevel: AuthLevel.HACKER });
 		if (!users) {
 			return [];
@@ -130,6 +134,7 @@ class HackerResolver {
 
 	/**
 	 * @param {number} number - number of top schools to return
+	 * @returns {Promise<SchoolCounts[]>} an array of SchoolCounts objects
 	 */
 	@Query(() => [SchoolCounts], {
 		description: 'Returns top schools with counts',
@@ -165,9 +170,50 @@ class HackerResolver {
 	}
 
 	/**
-	 * @param {string} email - email address of a particular user
-	 * @param {Status} newStatus - new status to assign to user
-	 * @returns {Status} new status of user or null if the hacker doesn't exist
+	 * Updates a Hacker.
+	 *
+	 * @param {string} email - The email address of the hacker to update
+	 * @param {UpdateUserInput} data - Data to update the provided hacker (only desired fields)
+	 * @throws an error if any of the Mongo calls fail
+	 * @returns {Promise<boolean>} true if successful
+	 *
+	 */
+	@Mutation(() => Boolean, {
+		description: 'Update a Hacker',
+	})
+	public static async updateHacker(
+		@Arg('email') email: string,
+		@Arg('data', { nullable: true }) data: UpdateHackerInput
+	): Promise<boolean> {
+		// Find the hacker to update
+		const user = await UserModel.findOne({ authLevel: AuthLevel.HACKER, email });
+
+		// Throw an error if no such user exists
+		if (!user) {
+			throw new Error('Hacker does not exist!');
+		}
+
+		// Filter out any undefined data
+		const filteredData: UpdateHackerInput = {};
+		Object.keys(data).forEach(key =>
+			key !== undefined ? ((filteredData as any)[key] = (data as any)[key]) : ''
+		);
+
+		// Attempt to update the hacker
+		try {
+			await HackerModel.updateOne({ user: user._id }, { $set: filteredData });
+		} catch (err) {
+			throw new Error('Hacker could not be updated!');
+		}
+
+		// If successful, return true
+		return true;
+	}
+
+	/**
+	 * @param {string} email - email address of a particular hacker
+	 * @param {Status} newStatus - new status to assign to hacker
+	 * @returns {Status} new status of hacker or null if the hacker doesn't exist
 	 */
 	@Mutation(() => Status, {
 		description: "Update a Hacker's status and return updated status",
@@ -177,8 +223,7 @@ class HackerResolver {
 		@Arg('newStatus') newStatus: Status
 	): Promise<Status | null> {
 		if (!Object.values(Status).includes(newStatus)) {
-			console.log('Incorrect newStatus arg');
-			return null;
+			throw new Error('Incorrect newStatus arg!');
 		}
 
 		const user = await UserModel.findOne({ authLevel: AuthLevel.HACKER, email });
@@ -225,6 +270,139 @@ class HackerResolver {
 			}
 		});
 		return newStatus;
+	}
+
+	/**
+	 * @param {string} email - email address of a particular hacker
+	 * @param {string} teamName - name of team to join
+	 * @throws {Error} if unsuccessful
+	 * @returns {Promise<boolean>} true if Hacker was able to join team
+	 */
+	@Mutation(() => Boolean, {
+		description: 'Allow hackers to join a team',
+	})
+	public static async joinTeam(
+		@Arg('email', { nullable: false }) email: string,
+		@Arg('teamName', { nullable: false }) teamName: string
+	): Promise<boolean> {
+		// Make sure the team and hacker exist
+		const team = await TeamModel.findOne({ teamName });
+		const user = await UserModel.findOne({ authLevel: AuthLevel.HACKER, email });
+
+		// If the user doesn't exist, throw an error
+		if (!user) {
+			throw new Error('User does not exist!');
+		}
+
+		// Grab the user's associated hacker object
+		const hacker = await HackerModel.findOne({ user: user._id });
+
+		// Ensure the hacker object isn't null
+		if (!hacker) {
+			throw new Error('Hacker does not exist!');
+		}
+
+		// If the team doesn't exist, create it
+		if (!team) {
+			try {
+				await TeamModel.create({ size: 1, teamMembers: [{ _id: hacker._id }], teamName });
+			} catch (err) {
+				throw new Error('Team could not be created!');
+			}
+		} else {
+			// Check if the user is already part of the team
+			if (team.teamMembers.indexOf(hacker._id) != -1) {
+				throw new Error('Hacker is already a part of this team!');
+			}
+
+			// Check if the team is full
+			if (team.size === CONSTANTS.MAX_TEAM_SIZE) {
+				throw new Error('This team is already full!');
+			}
+
+			// Add the hacker to the team
+			try {
+				await TeamModel.updateOne(
+					{ teamName },
+					{ $push: { teamMembers: { _id: hacker._id } }, $set: { size: team.size + 1 } }
+				);
+			} catch (err) {
+				throw new Error('Hacker could not be added to team!');
+			}
+		}
+
+		// Update the hacker's team
+		try {
+			await HackerModel.updateOne({ _id: hacker._id }, { $set: { teamName: teamName } });
+		} catch (err) {
+			throw new Error('Hacker team could not be updated!');
+		}
+
+		// Upon success, return true
+		return true;
+	}
+
+	/**
+	 * @param {string} email - email address of the user to add to a team
+	 * @throws {Error} if unsuccessful
+	 * @returns {Promise<boolean>} true if successful
+	 */
+	@Mutation(() => Boolean, {
+		description: 'Allow hackers to leave a team',
+	})
+	public static async leaveTeam(
+		@Arg('email', { nullable: false }) email: string
+	): Promise<boolean> {
+		// Ensure the team and hacker are in a valid state
+		const user = await UserModel.findOne({ authLevel: AuthLevel.HACKER, email });
+
+		// If the user doesn't exist, throw an error
+		if (!user) {
+			throw new Error('User does not exist!');
+		}
+
+		// Get the hacker's team name
+		const hacker = await HackerModel.findOne({ user: user._id });
+
+		if (!hacker) {
+			throw new Error('Hacker does not exist!');
+		} else if (!hacker.teamName) {
+			throw new Error('Hacker is not on a team!');
+		}
+
+		const team = await TeamModel.findOne({ teamName: hacker.teamName });
+
+		if (!team) {
+			throw new Error('Team does not exist!');
+		} else if (team.teamMembers.indexOf(hacker._id) === -1) {
+			throw new Error('Hacker is not on this Team!');
+		}
+
+		// Remove hacker from the team
+		try {
+			const updatedTeam = await TeamModel.findOneAndUpdate(
+				{ teamName: hacker.teamName },
+				{ $pull: { teamMembers: hacker._id }, $set: { size: team.size - 1 } },
+				{ new: true }
+			);
+
+			// Remove teamName from Hacker's profile
+			await HackerModel.updateOne({ _id: hacker._id }, { $unset: { teamName: '' } });
+
+			// If the team is now empty, delete it
+			if (updatedTeam && updatedTeam.size === 0) {
+				try {
+					await TeamModel.deleteOne({ teamName: hacker.teamName });
+				} catch (err) {
+					throw new Error('Now empty team could not be deleted!');
+				}
+			}
+		} catch (err) {
+			throw new Error('Hacker could not be removed from team!');
+		}
+
+		// Upon success, return true
+		return true;
 	}
 }
 
