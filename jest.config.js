d5327745ced2a490ff823fbccc30bf9ce87e2f4e
Ben Cooper <bencooper222@gmail.com>
2019-05-15 17:33:12 -0700

    add jest reporting
    update jest report path
    
    specifc job paths
    
    change jest write location
    
    attempt junit reporting
    
    try squiggly
    
    sighs
    
    fml
    
    specifies folder and file separately for jest-junit
    
    specify circle path
    
    more consistent naming

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,9 @@
 module.exports = {
-	roots: ['tests'],
+	// roots: ['tests'],
 	transform: {
 		'^.+\\.tsx?$': 'ts-jest',
 	},
+	// just specifying folder and filename in "output" option doesn't work for ?? reasons
+	reporters: ['default', ['jest-junit', { outputDirectory: './reports/jest', outputName: 'results.xml' }]],
 };
+
