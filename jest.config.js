6811b3474d4ea2a9dc3b5aa6fcf4f140b7933977
Ben Cooper <bencooper222@gmail.com>
2019-05-15 21:59:06 -0700

    match circle directory naming convention
    no random junit file

--- jest.config.js
+++ jest.config.js
@@ -4,6 +4,6 @@ module.exports = {
 		'^.+\\.tsx?$': 'ts-jest',
 	},
 	// just specifying folder and filename in "output" option doesn't work for ?? reasons
-	reporters: ['default', ['jest-junit', { outputDirectory: './reports/jest', outputName: 'results.xml' }]],
+	reporters: ['default', ['jest-junit', { outputDirectory: './reports/test/jest', outputName: 'results.xml' }]],
 };
 
