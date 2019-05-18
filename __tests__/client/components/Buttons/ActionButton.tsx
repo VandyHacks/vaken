94a67dc712517a7997e5fe4e564b1a8b072b5629
Ben Cooper <bencooper222@gmail.com>
2019-05-18 01:35:35 -0700

    add codecov to circle config
    remove duplicate circle version
    
    remove line break
    
    shift code coverage uploading
    
    change file coverage location

--- __tests__/client/components/Buttons/ActionButton.tsx
+++ __tests__/client/components/Buttons/ActionButton.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import renderer from 'react-test-renderer';
 import ActionButton from '../../../../src/client/components/Buttons/ActionButton';
 
-
 it('ActionButton', () => {
 	const component = renderer.create(<ActionButton />).toJSON();
 
