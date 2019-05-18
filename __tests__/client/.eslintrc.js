032ae7ad0b44682defd5933a2070edee5bf0c9d5
Ben Cooper <bencooper222@gmail.com>
2019-05-18 01:25:54 -0700

    add eslint jest

--- __tests__/client/.eslintrc.js
+++ __tests__/client/.eslintrc.js
@@ -1,1 +1,11 @@
-module.exports = require('../../src/client/.eslintrc'); // just use the frontend eslint config
+const frontendEslint = {
+    ...require('../../src/client/.eslintrc'),
+    env: {
+      'jest/globals': true,
+    },
+  }; // just use the frontend eslint config
+  
+  frontendEslint.plugins.push('jest');
+  
+  module.exports = frontendEslint;
+  
