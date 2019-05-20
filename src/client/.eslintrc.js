17900dc423fed18cbc368e11a85678cf59b6d0d6
Benjamin Cooper <bencooper222@gmail.com>
2019-05-20 16:39:44 -0700

    better align frontend lint config with backend

--- src/client/.eslintrc.js
+++ src/client/.eslintrc.js
@@ -1,11 +1,9 @@
 // for the client ONLY
 module.exports = {
 	extends: [
-		'plugin:@typescript-eslint/recommended', // Uses the recommended rules from the @typescript-eslint/eslint-plugin
-		'plugin:react/recommended',
 		'airbnb', // Airbnb style guide
-		'prettier/@typescript-eslint', // Uses eslint-config-prettier to disable ESLint rules from @typescript-eslint/eslint-plugin that would conflict with prettier
-		'plugin:prettier/recommended', // Enables eslint-plugin-prettier and displays prettier errors as ESLint errors. Make sure this is always the last configuration in the extends array.
+		'typescript',
+		'typescript/prettier-react'
 	],
 	settings: {
 		'import/resolver': {
