a3282147a74d6b7e3d7cfff39b4fa6021d649769
cktang88 <arctangent20@yahoo.com>
2019-05-23 16:51:46 -0500

    Start testing frontend

--- jest.config.js
+++ jest.config.js
@@ -2,9 +2,19 @@
 module.exports = {
 	collectCoverage: true,
 	collectCoverageFrom: ['./src/**/*.{ts,tsx}'],
+	moduleNameMapper: {
+		'.+\\.svg?.+$': '@svgr/webpack', // see https://github.com/smooth-code/svgr/issues/83
+		'react-virtualized/styles.css': 'jest-transform-css',
+	},
 	preset: 'ts-jest',
-	reporters: ['default', ['jest-junit', { outputDirectory: './reports/test/jest', outputName: 'results.xml' }]],
+	reporters: [
+		'default',
+		['jest-junit', { outputDirectory: './reports/test/jest', outputName: 'results.xml' }],
+	],
 	testEnvironment: 'node',
-	testPathIgnorePatterns: ["/node_modules/", ".eslintrc.js"],
+	testPathIgnorePatterns: ['/node_modules/', '.eslintrc.js'],
+	transform: {
+		'^.+\\.jsx?$': 'babel-jest',
+		'^.+\\.css$': 'jest-transform-css',
+	},
 };
-
