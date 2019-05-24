13eece944b1762f7215e08b0844cb5c72c2467e3
cktang88 <arctangent20@yahoo.com>
2019-05-23 20:18:57 -0500

    Initialize application test

--- __tests__/client/components/Sidebar/Sidebar.tsx
+++ __tests__/client/components/Sidebar/Sidebar.tsx
@@ -4,7 +4,7 @@ import { MemoryRouter } from 'react-router-dom';
 
 import Sidebar from '../../../../src/client/components/Sidebar/Sidebar';
 
-it('Sidebar', async () => {
+it('Sidebar renders properly', () => {
 	const component = renderer
 		.create(
 			// see https://github.com/ReactTraining/react-router/blob/master/packages/react-router/docs/guides/testing.md
