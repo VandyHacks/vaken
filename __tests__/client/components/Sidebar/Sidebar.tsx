6a6ef0c69492ba9a6ad0bb9d04d19246d2bb101f
cktang88 <arctangent20@yahoo.com>
2019-05-24 14:05:00 -0500

    Fix sidebar snapshot

--- __tests__/client/components/Sidebar/Sidebar.tsx
+++ __tests__/client/components/Sidebar/Sidebar.tsx
@@ -4,7 +4,7 @@ import { MemoryRouter } from 'react-router-dom';
 
 import Sidebar from '../../../../src/client/components/Sidebar/Sidebar';
 
-it('Sidebar renders properly', () => {
+it('Sidebar renders correctly', () => {
 	const component = renderer
 		.create(
 			// see https://github.com/ReactTraining/react-router/blob/master/packages/react-router/docs/guides/testing.md
