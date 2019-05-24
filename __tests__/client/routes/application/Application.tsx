e779032a5bbc9a93f11a62768225e98bce293ec9
cktang88 <arctangent20@yahoo.com>
2019-05-24 16:16:42 -0500

    Add application renders snapshot

--- __tests__/client/routes/application/Application.tsx
+++ __tests__/client/routes/application/Application.tsx
@@ -1,14 +1,11 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
-import { MemoryRouter } from 'react-router-dom';
+// import { MemoryRouter } from 'react-router-dom';
 
 import Application from '../../../../src/client/routes/application/Application';
-
-it('Application initializes', () => {
-	expect(Application).toBeTruthy();
-	/*
-	const component = renderer.create(<Application />).toJSON();
-
-    expect(component).toMatchSnapshot();
-    */
+describe('Test Application', () => {
+	it(' renders correctly', async () => {
+		const component = renderer.create(<Application />).toJSON();
+		expect(component).toMatchSnapshot();
+	});
 });
