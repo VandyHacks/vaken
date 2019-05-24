609baeae91da0c23fdae7ab791b1e4bc3742605d
cktang88 <arctangent20@yahoo.com>
2019-05-24 16:16:42 -0500

    Add application renders snapshot

--- jest.config.js
+++ jest.config.js
@@ -4,6 +4,8 @@ module.exports = {
 	collectCoverageFrom: ['./src/**/*.{ts,tsx}'],
 	moduleNameMapper: {
 		'../../assets/img/square_hackathon_logo.svg': '<rootDir>/__mocks__/svgrMock.js',
+		'../../assets/img/unchecked_box.svg': '<rootDir>/__mocks__/svgrMock.js',
+		'../../assets/img/checked_box.svg': '<rootDir>/__mocks__/svgrMock.js',
 		'.+\\.svg?.+$': '@svgr/webpack', // see https://github.com/smooth-code/svgr/issues/83
 		'react-virtualized/styles.css': 'jest-transform-css',
 
