d51ab56c561e0184d87128d2a4d50d05573d21ec
cktang88 <arctangent20@yahoo.com>
2019-05-24 17:05:01 -0500

    Mock universities

--- __tests__/client/routes/application/Application.tsx
+++ __tests__/client/routes/application/Application.tsx
@@ -3,6 +3,9 @@ import renderer from 'react-test-renderer';
 // import { MemoryRouter } from 'react-router-dom';
 
 import Application from '../../../../src/client/routes/application/Application';
+
+jest.mock('../../../../src/client/assets/data/institutions.json', () => ['Vanderbilt University']);
+
 describe('Test Application', () => {
 	it(' renders correctly', async () => {
 		const component = renderer.create(<Application />).toJSON();
