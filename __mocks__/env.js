3199ae54c8a3bf5f73cad594163496208573a449
cktang88 <arctangent20@yahoo.com>
2019-05-24 23:52:27 -0500

    Update env vars mock for CI

--- __mocks__/env.js
+++ __mocks__/env.js
@@ -1,3 +1,5 @@
 // assign process.env
-require('dotenv').config(); // read env file
-process.env.GOOGLE_CLIENT_ID = 'value';
+require('dotenv').config({ path: '../.env.template' }); // read env file (only works on local, not on CI)
+
+// fallbacks
+// process.env.GOOGLE_CLIENT_ID = 'value';
