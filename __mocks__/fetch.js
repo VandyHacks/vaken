ae37bc89b54ae2535fdb62e7e741eeabc66f0e49
cktang88 <arctangent20@yahoo.com>
2019-05-25 13:25:06 -0500

    Fix test using mock fetch

--- __mocks__/fetch.js
+++ __mocks__/fetch.js
@@ -1,9 +1,10 @@
 // mock browser fetch
 
 // NOTE: can't use async b/c babel transform only applies to test files, not setup files
-global.fetch = new Promise(() => ({
-	json: () => {
-		message: 'ok';
-	},
-	status: 200,
-}));
+global.fetch = () =>
+	new Promise(() => ({
+		json: () => {
+			message: 'ok';
+		},
+		status: 200,
+	}));
