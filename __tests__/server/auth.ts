6660da1fb90a00a4a5d193e06e4748d53206a5ab
cktang88 <arctangent20@yahoo.com>
2019-05-24 23:48:08 -0500

    Fix auth test for linting

--- __tests__/server/auth.ts
+++ __tests__/server/auth.ts
@@ -1,9 +1,21 @@
-describe('Test auth.ts', () => {
-	it('Fails without a clientID provided', () => {
-		try {
-			const passport = require('../../src/server/auth');
-		} catch (e) {
-			expect(e).toEqual(new TypeError('OAuth2Strategy requires a clientID option'));
-		}
+import passport from '../../src/server/auth';
+const MOCK_CB = jest.fn();
+beforeEach(() => {
+	// reset each time
+	jest.clearAllMocks();
+});
+
+describe('Test passport for authentication', () => {
+	it('authorizes with google strategy', async () => {
+		await passport.authenticate('google', MOCK_CB);
+		expect(MOCK_CB.mock.calls.length).toBe(0);
+	});
+	it('authorizes with github strategy', async () => {
+		await passport.authenticate('github', MOCK_CB);
+		expect(MOCK_CB.mock.calls.length).toBe(0);
+	});
+	it('authorizes with local strategy', async () => {
+		await passport.authenticate('local', MOCK_CB);
+		expect(MOCK_CB.mock.calls.length).toBe(0);
 	});
 });
