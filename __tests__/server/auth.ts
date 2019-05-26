f7347b2bb5b9da72dc0eabfbbbce029f6f532150
cktang88 <arctangent20@yahoo.com>
2019-05-25 23:24:12 -0500

    Add test for auth

--- __tests__/server/auth.ts
+++ __tests__/server/auth.ts
@@ -1,4 +1,6 @@
-import passport from '../../src/server/auth';
+import passport, { createStrategyHandler } from '../../src/server/auth';
+import AuthType from '../../src/server/enums/AuthType';
+import { Profile as GoogleProfile } from 'passport-google-oauth';
 
 const MOCK_CB = jest.fn();
 beforeEach(() => {
@@ -7,6 +9,12 @@ beforeEach(() => {
 });
 
 describe('Test passport for authentication', () => {
+	it('creates strategy handlers properly', async () => {
+		const handler = await createStrategyHandler(AuthType.GOOGLE);
+		await handler(null, '', '', ({} as unknown) as GoogleProfile, MOCK_CB);
+		expect(handler).toBeTruthy();
+		expect(MOCK_CB.mock.calls.length).toBe(1);
+	});
 	it('authorizes with google strategy', async () => {
 		await passport.authenticate('google', MOCK_CB);
 		expect(MOCK_CB.mock.calls.length).toBe(0);
