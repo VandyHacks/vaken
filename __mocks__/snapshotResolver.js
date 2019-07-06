4a90aecf972e72fbb81ee4383614b65164fb353a
cktang88 <arctangent20@yahoo.com>
2019-07-06 16:03:52 -0400

    Fix paths for windows

--- __mocks__/snapshotResolver.js
+++ __mocks__/snapshotResolver.js
@@ -1,16 +1,13 @@
 /* eslint-disable */
 const path = require('path');
-
 module.exports = {
 	/** resolves from test to snapshot path */
 	resolveSnapshotPath: (testPath, snapshotExtension) => {
-		return testPath.replace('src/', '__snapshots__/') + snapshotExtension;
+		return testPath.replace('src', '__snapshots__') + snapshotExtension;
 	},
-
 	/** resolves from snapshot to test path */
 	resolveTestPath: (snapshotFilePath, snapshotExtension) => {
-		return snapshotFilePath.replace('__snapshots__/', 'src/').slice(0, -snapshotExtension.length);
+		return snapshotFilePath.replace('__snapshots__', 'src').slice(0, -snapshotExtension.length);
 	},
-
 	testPathForConsistencyCheck: 'src/client/example/example.test.js',
 };
