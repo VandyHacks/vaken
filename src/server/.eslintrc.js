d9eedc7e4eab4ed556b9ed99d225dc1e568edbd0
cktang88 <arctangent20@yahoo.com>
2019-07-06 15:50:33 -0400

    Removed unused server eslint rules

--- src/server/.eslintrc.js
+++ src/server/.eslintrc.js
@@ -1,13 +1,4 @@
 // for the server ONLY
 module.exports = {
 	// NOTE: this stacks on top of common eslint rules
-	rules: {
-		'no-underscore-dangle': 'off', // messes with mongo _id
-		'no-param-reassign': 'off', // we often reassign "ctx" param properties for koa
-		//-----------------------------------------------------------//
-		// see https://github.com/bradzacher/eslint-plugin-typescript/blob/master/docs/rules/no-unused-vars.md#options
-		'no-unused-vars': 'off', // disable base rule
-		'@typescript-eslint/no-unused-vars': ['error', { vars: 'local' }], // local vars prevent exported enums from being declared unused
-		// ---------------------------------------------------------//
-	},
 };
