def0e22ad6c0fb5cde4206990fc038c455fbbdba
Matt Leon <matthew.leon@vanderbilt.edu>
2019-07-06 04:21:09 -0400

    Fix typecheck error

--- src/client/routes/profile/ProfileConfig.ts
+++ src/client/routes/profile/ProfileConfig.ts
@@ -2,7 +2,7 @@
 import { Gender, ShirtSize, DietaryRestriction } from '../../generated/graphql';
 import { Input as TextInput } from '../../components/Input/TextInput';
 import { Checkbox } from '../../components/Input/Checkbox';
-import { Slider } from '../../components/Input/Slider';
+import { Slider, SliderSansTextTransform } from '../../components/Input/Slider';
 // import { Boolean } from '../../components/Input/Boolean';
 // import Calendar from '../../components/Input/Calendar';
 
