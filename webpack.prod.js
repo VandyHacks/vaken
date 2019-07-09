a0c5dc303714bcd8f00750a11a420967864ad8a6
Benjamin Cooper <bencooper222@gmail.com>
2019-07-09 10:44:11 -0700

    Have webpack calculate integrity attribute for inline scripts

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,7 +1,14 @@
 const merge = require('webpack-merge');
 const common = require('./webpack.common.js');
+const SriPlugin = require('webpack-subresource-integrity');
 
 module.exports = merge.smart(common, {
 	mode: 'production',
 	devtool: 'source-map',
+	plugins: [
+		new SriPlugin({
+			hashFuncNames: ['sha384'],
+			enabled: true,
+		}),
+	],
 });
