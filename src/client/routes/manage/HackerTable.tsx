1dcbbb8268013ae7129d784e4b1439964f066358
cktang88 <arctangent20@yahoo.com>
2019-07-08 20:57:10 -0400

    Use separate component for hacker table data

--- src/client/routes/manage/HackerTable.tsx
+++ src/client/routes/manage/HackerTable.tsx
@@ -1,16 +1,5 @@
 import React, { useContext, useState, useEffect, useRef, FC } from 'react';
-import {
-	Table,
-	Column,
-	AutoSizer,
-	SortDirection,
-	SortIndicator,
-	TableHeaderProps,
-	TableCellProps,
-	TableRowProps,
-	SortDirectionType,
-	Index,
-} from 'react-virtualized';
+import { AutoSizer, SortDirection, SortDirectionType } from 'react-virtualized';
 import 'react-virtualized/styles.css';
 import styled from 'styled-components';
 import Fuse from 'fuse.js';
@@ -20,8 +9,6 @@ import { SelectableGroup, SelectAll, DeselectAll } from 'react-selectable-fast';
 
 import { ToggleSwitch } from '../../components/Buttons/ToggleSwitch';
 import { FloatingButton } from '../../components/Buttons/FloatingButton';
-import { Status } from '../../components/Text/Status';
-import { Checkmark } from '../../components/Symbol/Checkmark';
 import { SearchBox } from '../../components/Input/SearchBox';
 import STRINGS from '../../assets/strings.json';
 import { TableCtxI, TableContext, Option } from '../../contexts/TableContext';
@@ -31,58 +18,15 @@ import {
 	HackersQuery,
 	useHackerStatusesMutation,
 } from '../../generated/graphql';
-import { Row } from './Row';
-import actionRenderer from './ActionRenderer';
-import DeselectElement, { SliderInput } from './HackerTableHelper';
-import { reimbursementHeaderRenderer } from './ReimbursementHeader';
+
+import DeselectElement, { SliderInput } from './SliderInput';
+import { HackerTableRows } from './HackerTableRows';
 
 const Float = styled.div`
 	position: fixed;
 	bottom: 3.5rem;
 	right: 11.75rem;
 	margin-right: 1rem;
-`;
-
-// Removes unwanted highlighting, adds alternating row colors
-const StyledTable = styled(Table)`
-	.ReactVirtualized__Table__Grid {
-		:focus {
-			outline: none;
-			border: none;
-		}
-
-		overflow: hidden;
-	}
-
-	.headerRow {
-		font-size: 1rem;
-		text-transform: capitalize;
-		color: ${STRINGS.DARK_TEXT_COLOR};
-	}
-
-	.headerRow,
-	.evenRow,
-	.oddRow {
-		box-sizing: border-box;
-		border-bottom: 0.0625rem solid #e0e0e0;
-	}
-	.oddRow {
-		background-color: #fafafa;
-	}
-
-	.ReactVirtualized__Table__headerColumn {
-		:focus {
-			outline: none;
-		}
-	}
-
-	font-size: 0.8rem;
-	margin-bottom: 5rem;
-	color: ${STRINGS.DARKEST_TEXT_COLOR};
-
-	.selected {
-		background-color: #e5e7fa;
-	}
 `;
 
 const TableLayout = styled('div')`
@@ -163,56 +107,6 @@ const columnOptions: { label: string; value: keyof QueriedHacker }[] = [
 	{ label: 'Graduation Year', value: 'gradYear' },
 	{ label: 'Status', value: 'status' },
 ];
-
-// renders a text label with a clickable sort indicator
-const renderHeaderAsLabel = ({
-	dataKey,
-	sortBy,
-	sortDirection,
-	label,
-}: TableHeaderProps): JSX.Element => {
-	return (
-		<>
-			{label}
-			{sortBy === dataKey && <SortIndicator sortDirection={sortDirection} />}
-		</>
-	);
-};
-
-// renders a solid checkmark if true, else an empty circle
-const checkmarkRenderer = ({ cellData }: TableCellProps): JSX.Element => {
-	return <Checkmark value={cellData} />;
-};
-
-// wrapper to use createSelectable() from react-selectable-fast
-const rowRenderer = (
-	props: TableRowProps & { selectableRef: string; selected: boolean; selecting: boolean }
-): JSX.Element => {
-	return <Row {...props} />;
-};
-
-// mapping from status labels to the colored label images
-const statusRenderer = ({ cellData }: TableCellProps): JSX.Element => {
-	const generateColor = (value: ApplicationStatus): string => {
-		switch (value) {
-			case ApplicationStatus.Created:
-				return STRINGS.COLOR_PALETTE[0];
-			case ApplicationStatus.Started:
-				return STRINGS.COLOR_PALETTE[2];
-			case ApplicationStatus.Submitted:
-				return STRINGS.COLOR_PALETTE[3];
-			case ApplicationStatus.Accepted:
-				return STRINGS.COLOR_PALETTE[4];
-			case ApplicationStatus.Confirmed:
-				return STRINGS.COLOR_PALETTE[5];
-			case ApplicationStatus.Rejected:
-				return STRINGS.COLOR_PALETTE[6];
-			default:
-				return STRINGS.ACCENT_COLOR;
-		}
-	};
-	return <Status value={cellData} generateColor={generateColor} />;
-};
 
 // handles basic alphanumeric sorting
 const collator = new Intl.Collator('en', { numeric: true, sensitivity: 'base' });
@@ -446,81 +340,15 @@ const HackerTable: FC<HackerTableProps> = ({ data }: HackerTableProps): JSX.Elem
 							onSelectionFinish={onSelectionFinish(table)}
 							ignoreList={['.ignore-select']}
 							resetOnStart>
-							<StyledTable
+							<HackerTableRows
 								width={width}
 								height={height}
-								headerHeight={40}
-								rowHeight={30}
-								rowCount={sortedData.length}
-								rowClassName={generateRowClassName}
-								rowGetter={({ index }: Index) => sortedData[index]}
-								rowRenderer={rowRenderer}
-								headerClassName="ignore-select"
-								sort={onSortColumnChange(table)}
-								{...table.state}>
-								<Column
-									className="column"
-									label="First Name"
-									dataKey="firstName"
-									width={100}
-									headerRenderer={renderHeaderAsLabel}
-								/>
-								<Column
-									className="column"
-									label="Last Name"
-									dataKey="lastName"
-									width={100}
-									headerRenderer={renderHeaderAsLabel}
-								/>
-								<Column
-									className="column"
-									label="Email"
-									dataKey="email"
-									width={200}
-									headerRenderer={renderHeaderAsLabel}
-								/>
-								<Column
-									className="column"
-									label="Grad Year"
-									dataKey="gradYear"
-									width={100}
-									headerRenderer={renderHeaderAsLabel}
-								/>
-								<Column
-									className="column"
-									label="School"
-									dataKey="school"
-									width={175}
-									headerRenderer={renderHeaderAsLabel}
-								/>
-								<Column
-									className="column"
-									label="Status"
-									dataKey="status"
-									width={100}
-									minWidth={90}
-									headerRenderer={renderHeaderAsLabel}
-									cellRenderer={statusRenderer}
-								/>
-								<Column
-									className="column"
-									label="Requires Travel Reimbursement?"
-									dataKey="needsReimbursement"
-									width={30}
-									minWidth={20}
-									headerRenderer={reimbursementHeaderRenderer}
-									cellRenderer={checkmarkRenderer}
-								/>
-								<Column
-									className="column"
-									label="Actions"
-									dataKey="actions"
-									width={275}
-									minWidth={275}
-									headerRenderer={renderHeaderAsLabel}
-									cellRenderer={actionRenderer(updateStatus)}
-								/>
-							</StyledTable>
+								updateStatus={updateStatus}
+								sortedData={sortedData}
+								onSortColumnChange={onSortColumnChange}
+								generateRowClassName={generateRowClassName}
+								table={table}
+							/>
 							{selectAll || hasSelection ? (
 								<DeselectAll ref={deselect}>{SelectAllButton}</DeselectAll>
 							) : (
