63b0cbdf35b7186df3a178c2a4977fca76cb7175
cktang88 <arctangent20@yahoo.com>
2019-07-22 23:22:41 -0400

    Small refactor

--- src/populateDb.ts
+++ src/populateDb.ts
@@ -13,35 +13,40 @@ import {
 import modelsPromise from './server/models';
 
 const getRandom = (max: number): number => Math.floor(Math.random() * max);
+const NUM_HACKERS = 800;
+
+const generateHacker: () => HackerDbObject = () => {
+	const fn = faker.name.firstName();
+	const ln = faker.name.lastName();
+	return {
+		_id: new ObjectID(),
+		createdAt: new Date(),
+		dietaryRestrictions: [Object.values(DietaryRestriction)[getRandom(7)]],
+		email: faker.internet.email(),
+		firstName: fn,
+		gender: Object.values(Gender)[getRandom(7)],
+		gradYear: getRandom(4) + 2019,
+		lastName: ln,
+		logins: [],
+		majors: [],
+		modifiedAt: new Date().getTime(),
+		phoneNumber: faker.phone.phoneNumber(),
+		preferredName: fn,
+		race: [Object.values(Race)[getRandom(6)]],
+		school: institutions[getRandom(1430)],
+		secondaryIds: [],
+		shirtSize: Object.values(ShirtSize)[getRandom(6)],
+		status: Object.values(ApplicationStatus)[getRandom(7)],
+		userType: UserType.Hacker,
+	};
+};
 
 const addHackers = async (): Promise<void> => {
 	const models = await modelsPromise;
 	const newHackers: HackerDbObject[] = [];
-	for (let i = 0; i < 800; i += 1) {
-		const fn = faker.name.firstName();
-		const ln = faker.name.lastName();
+	for (let i = 0; i < NUM_HACKERS; i += 1) {
 		console.log(`Adding hacker ${i}`);
-		newHackers.push({
-			_id: new ObjectID(),
-			createdAt: new Date(),
-			dietaryRestrictions: [Object.values(DietaryRestriction)[getRandom(7)]],
-			email: faker.internet.email(),
-			firstName: fn,
-			gender: Object.values(Gender)[getRandom(7)],
-			gradYear: getRandom(4) + 2019,
-			lastName: ln,
-			logins: [],
-			majors: [],
-			modifiedAt: new Date().getTime(),
-			phoneNumber: faker.phone.phoneNumber(),
-			preferredName: fn,
-			race: [Object.values(Race)[getRandom(6)]],
-			school: institutions[getRandom(1430)],
-			secondaryIds: [],
-			shirtSize: Object.values(ShirtSize)[getRandom(6)],
-			status: Object.values(ApplicationStatus)[getRandom(7)],
-			userType: UserType.Hacker,
-		});
+		newHackers.push(generateHacker());
 	}
 
 	console.log(`Adding the hackers`);
