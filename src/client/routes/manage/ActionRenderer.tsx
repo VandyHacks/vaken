bf81181781f69c3a0aebb5d64c2420cd59ce4b2d
Matt Leon <matthew.leon@vanderbilt.edu>
2019-07-22 23:11:00 -0400

    Fix long function code smell

--- src/client/routes/manage/ActionRenderer.tsx
+++ src/client/routes/manage/ActionRenderer.tsx
@@ -17,30 +17,29 @@ const Actions = styled('div')`
 	display: flex;
 `;
 
+function convertApplicationStatus(status: ApplicationStatus): string {
+	switch (status) {
+		case ApplicationStatus.Accepted:
+			return 'Accept';
+		case ApplicationStatus.Rejected:
+			return 'Reject';
+		default:
+			return 'Undecided';
+	}
+}
+
 // action column that contains the actionable buttons
 export default function actionRenderer(
 	updateStatus: HackerStatusMutationFn
 ): (p: ActionRendererProps) => JSX.Element {
 	return function ActionRenderer({ rowData: { id, status } }: ActionRendererProps) {
-		let sliderValue: string;
-		switch (status) {
-			case ApplicationStatus.Accepted:
-				sliderValue = 'Accept';
-				break;
-			case ApplicationStatus.Rejected:
-				sliderValue = 'Reject';
-				break;
-			default:
-				sliderValue = 'Undecided';
-		}
-
 		return (
 			<Actions className="ignore-select">
 				<RadioSlider
 					option1="Accept"
 					option2="Undecided"
 					option3="Reject"
-					value={sliderValue}
+					value={convertApplicationStatus(status)}
 					onChange={(input: string) => {
 						const newStatus = processSliderInput(input);
 						updateStatus({ variables: { input: { id, status: newStatus } } });
