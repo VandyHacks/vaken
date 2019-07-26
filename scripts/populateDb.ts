2711e025a2548df6ab9591b232cbb39b1d1b61fb
Matt Leon <matthew.leon@vanderbilt.edu>
2019-07-26 00:15:40 -0400

    Make populatedb script less verbose

--- scripts/populateDb.ts
+++ scripts/populateDb.ts
@@ -46,14 +46,15 @@ const generateHacker: () => HackerDbObject = () => {
 const addHackers = async (): Promise<void> => {
 	const models = await initDb();
 	const newHackers: HackerDbObject[] = [];
-	for (let i = 0; i < NUM_HACKERS; i += 1) {
-		console.log(`Adding hacker ${i}`);
+	for (let i = 1; i <= NUM_HACKERS; i += 1) {
+		if (i % 100 === 0) console.log(`Creating hacker ${i}`);
 		newHackers.push(generateHacker());
 	}
 
-	console.log(`Adding the hackers`);
+	console.log(`Adding the hackes to the DB...`);
 	const { insertedCount } = await models.Hackers.insertMany(newHackers);
 	console.log(`Inserted ${insertedCount} new hackers`);
+	process.exit(0);
 };
 
 addHackers();
