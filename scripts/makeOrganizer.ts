a829f3b432e399e96e012b0758ecf6a28f3ee8fe
Matt Leon <matthew.leon@vanderbilt.edu>
2019-08-01 00:35:08 -0400

    Allow batch processing of makeOrganizer

--- scripts/makeOrganizer.ts
+++ scripts/makeOrganizer.ts
@@ -1,28 +1,20 @@
 import { ObjectId } from 'mongodb';
-import modelsPromise from '../src/server/models';
+import { initDb, Models } from '../src/server/models';
 import { LoginDbObject, UserType } from '../src/server/generated/graphql';
 
 const EMAIL_REGEX = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
-const printUsage = (): void =>
+const printUsage = (): void => {
+	// eslint-disable-next-line no-console
 	void console.log(
-		`Usage: ts-node -r dotenv/config ./scripts/makeOrganizer.ts -- YOUR_EMAIL@FOO.COM [github] [google]`
+		`Usage: ts-node -r dotenv/config ./scripts/makeOrganizer.ts -- YOUR_EMAIL@FOO.COM,BEN@FOO.COM [github | google]`
 	);
+};
 
-(async (): Promise<void> => {
-	const args = process.argv.splice(process.execArgv.length);
-	if (!args.length || args.length > 2 || !EMAIL_REGEX.test(args[0])) printUsage();
-	const models = await modelsPromise;
-
-	const constraint: Partial<LoginDbObject> = {
-		email: args[0],
-		provider: args[1] || undefined,
-	};
-
-	const user = await models.Hackers.findOne({ email: args[0] });
+const makeOrganizer = async (models: Models, constraint: Partial<LoginDbObject>): Promise<void> => {
+	const user = await models.Hackers.findOne({ email: constraint.email });
 	if (!user) {
-		console.error(`Couldn't find your user... did you try logging in first?`);
-		return void process.exit(1);
+		throw new Error(`Couldn't find user ${constraint.email} did you try logging in first?`);
 	}
 
 	// Update the user's login entry to point to organizers.
@@ -32,8 +24,7 @@ const printUsage = (): void =>
 		{ returnOriginal: false }
 	);
 	if (!ok || !value) {
-		console.error(lastErrorObject);
-		return void process.exit(2);
+		throw new Error(`Error updating logins: ${JSON.stringify(lastErrorObject)}`);
 	}
 
 	// Only insert user into organizers collection if changing login was successful.
@@ -44,10 +35,32 @@ const printUsage = (): void =>
 		permissions: [],
 	});
 	if (!result.ok) {
-		console.error('Error: (Unspecified) while inserting user into organizers collection');
-		return void process.exit(3);
+		throw new Error('Error: (Unspecified) while inserting user into organizers collection');
 	}
 
-	console.log(`${args[0]} is now an organizer :) Happy hacking!`);
+	// eslint-disable-next-line no-console
+	console.log(`${constraint.email} is now an organizer :) Happy hacking!`);
+};
+
+(async (): Promise<void> => {
+	const args = process.argv.splice(process.execArgv.length);
+	if (!args.length || args.length > 2 || !EMAIL_REGEX.test(args[0])) printUsage();
+	const models = await initDb();
+
+	try {
+		await Promise.all(
+			args[0].split(',').map(email => {
+				const constraint: Partial<LoginDbObject> = args[1]
+					? { email, provider: args[1] }
+					: { email };
+
+				return makeOrganizer(models, constraint);
+			})
+		);
+	} catch (e) {
+		console.error(e);
+		process.exit(1);
+	}
+
 	return void process.exit();
 })();
