7dc8f90111c5802ff64c64e7c8e89fbf2ee87e93
Matt Leon <matthew.leon@vanderbilt.edu>
2019-08-10 21:32:06 -0400

    Rip out react-apollo-hooks and replace with @apollo/react-hooks

--- src/client/index.tsx
+++ src/client/index.tsx
@@ -1,8 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import ApolloClient from 'apollo-boost';
-import { ApolloProvider as ApolloHooksProvider } from 'react-apollo-hooks';
-import { ApolloProvider } from 'react-apollo';
+import { ApolloProvider } from '@apollo/react-hooks';
 import Vaken from './app';
 
 const client = new ApolloClient({ uri: '/graphql' });
@@ -10,9 +9,7 @@ const client = new ApolloClient({ uri: '/graphql' });
 const app = document.getElementById('App');
 ReactDOM.render(
 	<ApolloProvider client={client}>
-		<ApolloHooksProvider client={client}>
-			<Vaken />
-		</ApolloHooksProvider>
+		<Vaken />
 	</ApolloProvider>,
 	app
 );
