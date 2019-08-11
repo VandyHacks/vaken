d59d38cb8bb4b2090c43cd2adddd07293b46d870
Matt Leon <matthew.leon@vanderbilt.edu>
2019-08-11 01:30:55 -0400

    Add loading spinner to app submission

--- src/client/routes/application/Application.test.tsx
+++ src/client/routes/application/Application.test.tsx
@@ -1,13 +1,37 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
+import { MockedProvider } from '@apollo/react-testing';
+import { MyApplicationDocument } from '../../generated/graphql';
 
 import { Application } from './Application';
 
 jest.mock('../../assets/data/institutions.json', () => ['Vanderbilt University']);
 
+const mocks = [
+	{
+		request: {
+			query: MyApplicationDocument,
+		},
+		result: {
+			data: {
+				me: {
+					application: [{ answer: 'testing', question: 'First Name' }],
+					id: 'foo',
+				},
+			},
+		},
+	},
+];
+
 describe('Test Application', () => {
 	it(' renders correctly', async () => {
-		const component = renderer.create(<Application />).toJSON();
+		const component = renderer
+			.create(
+				<MockedProvider mocks={mocks}>
+					<Application />
+				</MockedProvider>
+			)
+			.toJSON();
 		expect(component).toMatchSnapshot();
 	});
 });
