4e94cecf0ba68bf5a2b12168fa93dff1e4c6dd92
kenneth <kenkraken@gmail.com>
2019-09-18 10:27:24 -0500

    Add EventsCheckIn type

--- src/server/nfc/index.ts
+++ src/server/nfc/index.ts
@@ -1,11 +1,12 @@
 import { UserInputError, AuthenticationError } from 'apollo-server-express';
-import { ObjectID } from 'mongodb';
+import { ObjectID, FindAndModifyWriteOpResultObject } from 'mongodb';
 import {
 	HackerDbObject,
 	OrganizerDbObject,
 	EventDbObject,
 	ShirtSize,
 	UserDbInterface,
+	UserInput,
 	UserType,
 } from '../generated/graphql';
 import Context from '../context';
@@ -18,3 +19,41 @@ import {
 	checkIsAuthorized,
 } from '../resolvers/helpers';
 import { Models } from '../models';
+
+export async function checkIfNFCIDExisted(
+	nfcID: string,
+	models: Models
+): Promise<HackerDbObject | null> {
+	const hacker = await models.Hackers.findOne({
+		secondaryIds: nfcID,
+	});
+	return hacker;
+}
+
+export async function getUserIDfromActiveNFCID(
+	nfcID: string,
+	models: Models
+): Promise<UserDbInterface | null> {
+	const user = models.Hackers.findOne({
+		$match: nfcID,
+		secondaryIds: { $slice: -1 },
+	});
+	return user;
+}
+
+export async function checkInUserToEvent(nfcID: string, event: string, models: Models) {
+	const hackerObj = getUserIDfromActiveNFCID(nfcID, models);
+	const eventObj = models.Events.findOne({ event });
+	const eventReturn = await new Promise(() =>
+		models.Events.findOneAndUpdate(
+			{ id: event },
+			{ $addToSet: { attendees: { id: hackerObj, timestamp: Date.now() } } }
+		)
+	).then(() =>
+		models.Events.findOneAndUpdate(
+			{ id: event },
+			{ $push: { checkins: { id: hackerObj, timestamp: Date.now() } } }
+		)
+	);
+	return eventReturn;
+}
