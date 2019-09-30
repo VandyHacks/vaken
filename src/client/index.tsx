5468f4cb0be5f5d7dbb976ce4eb57c3611ac2803
Matt Leon <matthew.leon@vanderbilt.edu>
2019-09-29 23:39:10 -0500

    Add website title

--- src/client/index.tsx
+++ src/client/index.tsx
@@ -3,14 +3,11 @@ import ReactDOM from 'react-dom';
 import ApolloClient from 'apollo-boost';
 import { ApolloProvider } from '@apollo/react-hooks';
 import Vaken from './app';
+import STRINGS from './assets/strings.json';
 
 const client = new ApolloClient({ uri: '/graphql' });
 
-document.addEventListener('focus', () => {
-	const body = document.getElementsByTagName('body')[0];
-	body.scrollTop = 0;
-	body.scrollLeft = 0;
-});
+window.document.title = STRINGS.WEBSITE_HTML_TITLE;
 
 const app = document.getElementById('App');
 ReactDOM.render(
