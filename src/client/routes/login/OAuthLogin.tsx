ff4c2a11385fc934f7606c7e1e85b11abeab1469
Benjamin Cooper <bencooper222@gmail.com>
2019-10-05 14:18:38 -0500

    Add msft logo to frontend

--- src/client/routes/login/OAuthLogin.tsx
+++ src/client/routes/login/OAuthLogin.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import LeftImgButton from '../../components/Buttons/LeftImgButton';
 import googleLogo from '../../assets/img/google_logo.svg';
 import githubLogo from '../../assets/img/github_logo.svg';
+import msftLogo from '../../assets/img/msft_logo.svg';
 import STRINGS from '../../assets/strings.json';
 
 const OAuthLogin = (): JSX.Element => {
@@ -16,6 +17,12 @@ const OAuthLogin = (): JSX.Element => {
 			<a href="/api/auth/google" style={{ textDecoration: 'none' }}>
 				<LeftImgButton color={STRINGS.DARK_TEXT_COLOR} img={googleLogo} imgAlt="Google logo">
 					Sign in with Google
+				</LeftImgButton>
+			</a>
+
+			<a href="/api/auth/microsoft" style={{ textDecoration: 'none' }}>
+				<LeftImgButton color={STRINGS.DARK_TEXT_COLOR} img={msftLogo} imgAlt="Microsoft logo">
+					Sign in with Microsoft
 				</LeftImgButton>
 			</a>
 		</>
