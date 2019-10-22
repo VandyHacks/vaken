38ea94107fad18f4fdafe5d73d951cdef692cf0f
benredmond <bjr0206@gmail.com>
2019-10-22 17:45:06 -0500

    Linted

--- src/client/routes/manage/HackerTable.tsx
+++ src/client/routes/manage/HackerTable.tsx
@@ -388,8 +388,8 @@ const HackerTable: FC<HackerTableProps> = ({
 									onClick={onRemoveSearchCriterion(table, index)}
 								/>
 							) : (
-									<div style={{ width: 'calc(10px + 2rem)' }} />
-								)}
+								<div style={{ width: 'calc(10px + 2rem)' }} />
+							)}
 						</FlexRow>
 					))}
 				</FlexColumn>
@@ -434,19 +434,19 @@ const HackerTable: FC<HackerTableProps> = ({
 							{selectAll || hasSelection ? (
 								<DeselectAll ref={deselect}>{SelectAllButton}</DeselectAll>
 							) : (
-									<SelectAll
-										onClick={() =>
-											table.update(draft => {
-												draft.hasSelection = true;
-												draft.selectedRowsIds = sortedData
-													.filter(row => isSelectable(row.status))
-													.map(row => row.id);
-												console.log(draft.selectedRowsIds);
-											})
-										}>
-										{SelectAllButton}
-									</SelectAll>
-								)}
+								<SelectAll
+									onClick={() =>
+										table.update(draft => {
+											draft.hasSelection = true;
+											draft.selectedRowsIds = sortedData
+												.filter(row => isSelectable(row.status))
+												.map(row => row.id);
+											console.log(draft.selectedRowsIds);
+										})
+									}>
+									{SelectAllButton}
+								</SelectAll>
+							)}
 							{hasSelection && (
 								<Float className="ignore-select">
 									<SliderInput
