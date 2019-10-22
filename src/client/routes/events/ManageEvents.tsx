bbb2c911d301abfa378c116ec71f00457a529928
kenneth <kenkraken@gmail.com>
2019-10-22 17:51:53 -0500

    Add tests to events backend and refactor

--- src/client/routes/events/ManageEvents.tsx
+++ src/client/routes/events/ManageEvents.tsx
@@ -8,25 +8,30 @@ import ActionButton from '../../components/Buttons/ActionButton';
 import { GraphQLErrorMessage } from '../../components/Text/ErrorMessage';
 import STRINGS from '../../assets/strings.json';
 import { defaultTableState, TableContext } from '../../contexts/TableContext';
-import { useAddOrUpdateEventMutation } from '../../generated/graphql';
+import { useAddOrUpdateEventMutation, useEventsQuery } from '../../generated/graphql';
 import { updateEventsHandler } from './helpers';
 import { EventUpdate } from './ManageEventTypes';
 
 const ManageEvents: FunctionComponent = (): JSX.Element => {
-	const [output, setOutput] = useState('sdfsdf');
+	const [output, setOutput] = useState('<>');
+	const [allEvents, setAllEvents] = useState('<all events>');
 	const [addOrUpdateEvent] = useAddOrUpdateEventMutation();
+	// const { loading, error, data } = useEventsQuery();
 
 	async function getEvents() {
 		const res = await fetch('/api/manage/events/pull');
-		const data = await res.json();
-		const eventsList = (await data) as EventUpdate[];
+		const resData = await res.json();
+		const eventsList = (await resData) as EventUpdate[];
 		const updatedEvents = updateEventsHandler(eventsList, addOrUpdateEvent);
 		setOutput(JSON.stringify(updatedEvents, null, '\t'));
+		// setAllEvents(data.events);
 	}
+
 	return (
 		<FloatingPopup>
 			<ActionButton onClick={getEvents}>Pull from Calendar</ActionButton>
 			<FloatingPopup>
+				<h1>Updated Events</h1>
 				<pre>{output}</pre>
 			</FloatingPopup>
 		</FloatingPopup>
