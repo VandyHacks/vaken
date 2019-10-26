f333a29016f1d81eb1f597bcab023eb20af21c3b
kenneth <kenkraken@gmail.com>
2019-10-25 22:12:53 -0500

    Clean up lint, rebase artifacts

--- src/client/routes/events/helpers.ts
+++ src/client/routes/events/helpers.ts
@@ -5,7 +5,7 @@ export function updateEventsHandler(
 	addOrUpdateEventFunction: AddOrUpdateEventMutationFn
 ): string[] {
 	if (events) {
-		events.map(event =>
+		events.forEach(event =>
 			addOrUpdateEventFunction({
 				variables: {
 					input: {
@@ -14,6 +14,7 @@ export function updateEventsHandler(
 				},
 			})
 		);
+		return events.map(event => event.name);
 	}
 	return [];
 }
