5a5622b09190a1a6d835d39afd17ce8de12ca24c
cktang88 <arctangent20@yahoo.com>
2019-11-13 17:46:17 -0600

    Add cors

--- src/server/index.ts
+++ src/server/index.ts
@@ -3,6 +3,7 @@ import { DIRECTIVES } from '@graphql-codegen/typescript-mongodb';
 import express from 'express';
 import passport from 'passport';
 import session from 'express-session';
+import cors, { CorsOptions } from 'cors';
 import MongoStore, { MongoUrlOptions } from 'connect-mongo';
 import gqlSchema from '../common/schema.graphql';
 import { resolvers } from './resolvers';
@@ -14,10 +15,13 @@ import { UnsubscribeHandler } from './mail/handlers';
 import { UserDbInterface } from './generated/graphql';
 import { pullCalendar } from './events';
 
-const { SESSION_SECRET, PORT, CALENDARID } = process.env;
+const { SESSION_SECRET, PORT, CALENDARID, NODE_ENV, PROD_ORIGIN } = process.env;
 if (!SESSION_SECRET) throw new Error(`SESSION_SECRET not set`);
 if (!PORT) throw new Error(`PORT not set`);
 if (!CALENDARID) logger.info('CALENDARID not set; skipping ical integration');
+if (!PROD_ORIGIN) throw new Error(`PROD_ORIGIN not set`)
+console.log(`Node env: ${process.env.NODE_ENV}`)
+
 const app = express();
 
 export const schema = makeExecutableSchema({
@@ -78,10 +82,30 @@ export const schema = makeExecutableSchema({
 		},
 		introspection: true, // OFF by default in prod, needs to be set true to remove compile errors
 		// playground: process.env.NODE_ENV !== 'production', // by DEFAULT, enabled when not in prod + disabled in prod
-		schema,
+		schema
 	});
 
-	server.applyMiddleware({ app });
+	const allowedOrigin = process.env.NODE_ENV === 'production' ? process.env.PROD_ORIGIN || '' : '';
+	console.log(`Allowed origins: ${allowedOrigin}`)
+
+	const corsOptions: CorsOptions = {
+		origin: function (requestOrigin, cb) {
+			if (!requestOrigin) {
+				console.error('Request origin missing, not allowed by CORS');
+				return
+			}
+			const allowed = requestOrigin.endsWith(allowedOrigin)
+
+			console.log(requestOrigin, allowed)
+			if (!allowed) {
+				console.error('Not allowed by CORS');
+				return
+			}
+			cb(null, allowed);
+		}
+	};
+
+	server.applyMiddleware({ app, cors: corsOptions });
 
 	if (process.env.NODE_ENV === 'production') {
 		// Serve front-end asset files in prod.
