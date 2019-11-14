99368c306a0604adc141e4f38d53c3e2a3f56f36
cktang88 <arctangent20@yahoo.com>
2019-11-13 23:18:30 -0600

    Revert webpack changes

--- webpack.common.js
+++ webpack.common.js
@@ -1,22 +1,13 @@
 const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
-const threadLoader = require('thread-loader');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
 
 const ASSET_PATH = process.env.ASSET_PATH || '/';
 
-threadLoader.warmup({
-	workers: 2
-}, [
-	// modules to load
-	// can be any module, i. e.
-	'babel-loader',
-]);
-
 module.exports = {
 	context: __dirname, // to automagically find tsconfig.json
 	entry: ['./src/client/index'],
-	stats: 'normal',
+	stats: 'minimal',
 	module: {
 		rules: [
 			{
@@ -26,9 +17,6 @@ module.exports = {
 				use: [
 					{
 						loader: 'thread-loader',
-						options: {
-							workers: 2
-						}
 					},
 					{
 						loader: 'babel-loader',
