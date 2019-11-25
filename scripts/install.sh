5034853d706a39fa68cc99f8b1f3fb032ed454ef
cktang88 <arctangent20@yahoo.com>
2019-11-24 23:09:20 -0600

    Remove nodemon, fix tsconfig

--- scripts/install.sh
+++ scripts/install.sh
@@ -2,7 +2,7 @@
 # Sets up Vaken developer environment. Please run in the top level of the Vaken repository.
 
 ENVFILE=".env"
-GLOBAL_DEPS=("nodemon" "webpack" "webpack-cli" "typescript" "ts-node")
+GLOBAL_DEPS=("webpack" "webpack-cli" "typescript" "ts-node")
 
 cp "$ENVFILE.template" "$ENVFILE"
 echo "Please update $ENVFILE with valid environment variables."
