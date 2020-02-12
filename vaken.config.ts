c40f4eed7cee8ef7c7bf1c8d264dd84d88a68a65
Gabriel Ting <gabrielfting@gmail.com>
2020-02-12 12:51:18 -0600

    Fix .env typing issues

--- vaken.config.ts
+++ vaken.config.ts
@@ -1,12 +1,17 @@
 /* eslint-disable */
 
-import { GoogleOauth } from '../vaken-plugin-google';
 import { verifyCallback } from './src/server/auth/helpers';
-const { GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET, GOOGLE_CALLBACK_URL } = process.env;
+
+import { GoogleOAuth } from '../vaken-plugin-google';
+
+// import Google OAuth requirements
+const GOOGLE_CLIENT_ID = process.env.GOOGLE_CLIENT_ID as string;
+const GOOGLE_CLIENT_SECRET = process.env.GOOGLE_CLIENT_SECRET as string;
+const GOOGLE_CALLBACK_URL = process.env.GOOGLE_CALLBACK_URL as string;
 
 export default [
 	{
-		package: new GoogleOauth({
+		package: new GoogleOAuth({
 			permissions: [],
 			settings: { GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET, GOOGLE_CALLBACK_URL, verifyCallback },
 		}),
