d1444cc38dd7277618b0885c9056b2868b0b6735
Gabriel Ting <gabrielfting@gmail.com>
2020-03-27 14:05:19 -0500

    Remove direct webpack plugin

--- webpack.prod.js
+++ webpack.prod.js
@@ -6,15 +6,15 @@ const PacktrackerPlugin = require('@packtracker/webpack-plugin');
 module.exports = merge.smart(common, {
 	mode: 'production', // by default minifies w/ terser
 	devtool: 'source-map',
-	plugins: [
-		// new SriPlugin({
-		// 	hashFuncNames: ['sha384'],
-		// 	enabled: true,
-		// }),
-		new PacktrackerPlugin({
-			project_token: process.env.PACKTRACKER_TOKEN,
-			upload: process.env.CI,
-			fail_build: true,
-		}),
-	],
+	// plugins: [
+	// 	// new SriPlugin({
+	// 	// 	hashFuncNames: ['sha384'],
+	// 	// 	enabled: true,
+	// 	// }),
+	// 	new PacktrackerPlugin({
+	// 		project_token: process.env.PACKTRACKER_TOKEN,
+	// 		upload: process.env.CI,
+	// 		fail_build: true,
+	// 	}),
+	// ],
 });
