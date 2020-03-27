3e99f84c8e9c166c5d71ebe81b7bf2994abadec8
Gabriel Ting <gabrielfting@gmail.com>
2020-03-27 14:07:27 -0500

    Remove unused import in webpack config file

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,7 +1,7 @@
 const merge = require('webpack-merge');
 const common = require('./webpack.common.js');
 // const SriPlugin = require('webpack-subresource-integrity');
-const PacktrackerPlugin = require('@packtracker/webpack-plugin');
+// const PacktrackerPlugin = require('@packtracker/webpack-plugin');
 
 module.exports = merge.smart(common, {
 	mode: 'production', // by default minifies w/ terser
