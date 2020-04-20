0bb69ea7ab006e8db46f1b9cf14cef21c86a46f5
Gabriel Ting <gabrielfting@gmail.com>
2020-04-19 23:26:46 -0500

    Refactor OAuth logos in client

--- src/client/plugins.ts
+++ src/client/plugins.ts
@@ -2,8 +2,10 @@
 
 import { NFCPlugin } from '../../plugins/nfc/client';
 import GoogleLogo from '@vandyhacks/google-oauth/logo.svg';
-
 import GitHubLogo from '@vandyhacks/github-oauth/logo.svg';
 
 export const packages = [new NFCPlugin()];
-export const authLogos = { google: GoogleLogo, github: GitHubLogo };
+export const authLogos = [
+	{ name: 'google', displayName: 'Google', logo: GoogleLogo },
+	{ name: 'github', displayName: 'GitHub', logo: GitHubLogo },
+];
--- src/client/routes/login/OAuthLogin.tsx
+++ src/client/routes/login/OAuthLogin.tsx
@@ -1,35 +1,46 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import LeftImgButton from '../../components/Buttons/LeftImgButton';
-import googleLogo from '../../assets/img/google_logo.svg';
-import githubLogo from '../../assets/img/github_logo.svg';
-import msftLogo from '../../assets/img/msft_logo.svg';
 import STRINGS from '../../assets/strings.json';
 
 import { authLogos } from '../../plugins';
 
+// const OAuthLogin = (): JSX.Element => {
+// 	const [providers, setProviders] = useState([]);
+
+// 	useEffect(() => {
+// 		const fetchProviders = async (): Promise<void> => {
+// 			const data = await (await fetch('/api/auth')).json();
+
+// 			setProviders(data);
+// 			console.log(data);
+// 		};
+
+// 		fetchProviders();
+// 	}, []);
+
+// 	return (
+// 		<>
+// 			{providers.map(({ name, displayName, svgPath }) => (
+// 				<a href={`/api/auth/${name}`} style={{ textDecoration: 'none' }} key={displayName}>
+// 					<LeftImgButton
+// 						color={STRINGS.DARK_TEXT_COLOR}
+// 						// img={`data:image/svg+xml,${svgPath}`}
+// 						img="data:text/plain;charset=utf-8;base64,PHN2ZyB3aWR0aD0iMjYiIGhlaWdodD0iMjYiIHZpZXdCb3g9IjAgMCAyNiAyNiIgZmlsbD0ibm9uZSIgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIj48cGF0aCBkPSJNMjUuODU3MiAxMC42MzU3SDEzLjI2NDFWMTUuOTU0M0gyMC40MDE4QzE5LjI2MDUgMTkuNSAxNi40NDIxIDIwLjY4MTQgMTMuMjA2NCAyMC42ODE0QzkuNzYwMjcgMjAuNjg1NyA2LjcyMDE4IDE4LjQ1OTkgNS43Mjk4MSAxNS4yMDc2QzQuNzM5NDMgMTEuOTU1MiA2LjAzMzc5IDguNDQ4MDYgOC45MTMwNyA2LjU4MjI2QzExLjc5MjMgNC43MTY0NiAxNS41NzMzIDQuOTM0ODEgMTguMjExNiA3LjExOTI0TDIyLjEzMzQgMy40Mzg2NEMxNy43ODk2IC0wLjUwNDA4IDExLjMxNTcgLTEuMTIyMDYgNi4yODQ2MSAxLjkyNTc0QzEuMjUzNDkgNC45NzM1NSAtMS4xMDkxIDEwLjk0NDYgMC41MDA3MDMgMTYuNTQzNUMyLjExMDUgMjIuMTQyNSA3LjMwMDQ5IDI2LjAwNTIgMTMuMjA2NCAyNkMyMC40ODA5IDI2IDI3LjA1OTUgMjEuMjcyOSAyNS44NTcyIDEwLjYzNTdaIiBmaWxsPSIjM0YzMzU2Ii8+PC9zdmc+"
+// 						imgAlt={`${displayName} logo`}>
+// 						{`Sign in with ${displayName}`}
+// 					</LeftImgButton>
+// 				</a>
+// 			))}
+// 		</>
+// 	);
+// };
+
 const OAuthLogin = (): JSX.Element => {
-	const [providers, setProviders] = useState([]);
-
-	useEffect(() => {
-		const fetchProviders = async (): Promise<void> => {
-			const data = await (await fetch('/api/auth')).json();
-
-			setProviders(data);
-			console.log(data);
-		};
-
-		fetchProviders();
-	}, []);
-
 	return (
 		<>
-			{providers.map(({ name, displayName, svgPath }) => (
+			{authLogos.map(({ name, displayName, logo }) => (
 				<a href={`/api/auth/${name}`} style={{ textDecoration: 'none' }} key={displayName}>
-					<LeftImgButton
-						color={STRINGS.DARK_TEXT_COLOR}
-						// img={`data:image/svg+xml,${svgPath}`}
-						img="data:text/plain;charset=utf-8;base64,PHN2ZyB3aWR0aD0iMjYiIGhlaWdodD0iMjYiIHZpZXdCb3g9IjAgMCAyNiAyNiIgZmlsbD0ibm9uZSIgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIj48cGF0aCBkPSJNMjUuODU3MiAxMC42MzU3SDEzLjI2NDFWMTUuOTU0M0gyMC40MDE4QzE5LjI2MDUgMTkuNSAxNi40NDIxIDIwLjY4MTQgMTMuMjA2NCAyMC42ODE0QzkuNzYwMjcgMjAuNjg1NyA2LjcyMDE4IDE4LjQ1OTkgNS43Mjk4MSAxNS4yMDc2QzQuNzM5NDMgMTEuOTU1MiA2LjAzMzc5IDguNDQ4MDYgOC45MTMwNyA2LjU4MjI2QzExLjc5MjMgNC43MTY0NiAxNS41NzMzIDQuOTM0ODEgMTguMjExNiA3LjExOTI0TDIyLjEzMzQgMy40Mzg2NEMxNy43ODk2IC0wLjUwNDA4IDExLjMxNTcgLTEuMTIyMDYgNi4yODQ2MSAxLjkyNTc0QzEuMjUzNDkgNC45NzM1NSAtMS4xMDkxIDEwLjk0NDYgMC41MDA3MDMgMTYuNTQzNUMyLjExMDUgMjIuMTQyNSA3LjMwMDQ5IDI2LjAwNTIgMTMuMjA2NCAyNkMyMC40ODA5IDI2IDI3LjA1OTUgMjEuMjcyOSAyNS44NTcyIDEwLjYzNTdaIiBmaWxsPSIjM0YzMzU2Ii8+PC9zdmc+"
-						imgAlt={`${displayName} logo`}>
+					<LeftImgButton color={STRINGS.DARK_TEXT_COLOR} img={logo} imgAlt={`${displayName} logo`}>
 						{`Sign in with ${displayName}`}
 					</LeftImgButton>
 				</a>
