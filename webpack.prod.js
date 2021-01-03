c149ae04b8cb94795218e96a5b7d0fe300b0cded
Benjamin Cooper <bencooper222@gmail.com>
2021-01-03 14:52:09 -0600

    Remove SRI dependency (#780)
    * remove dep
    
    * no plugins
    
    * bad package lock

--- webpack.prod.js
+++ webpack.prod.js
@@ -6,15 +6,4 @@ const common = require('./webpack.common.js');
 module.exports = merge(common, {
 	mode: 'production', // by default minifies w/ terser
 	devtool: 'source-map',
-	// plugins: [
-	// 	// new SriPlugin({
-	// 	// 	hashFuncNames: ['sha384'],
-	// 	// 	enabled: true,
-	// 	// }),
-	// 	new PacktrackerPlugin({
-	// 		project_token: process.env.PACKTRACKER_TOKEN,
-	// 		upload: process.env.CI,
-	// 		fail_build: true,
-	// 	}),
-	// ],
 });
