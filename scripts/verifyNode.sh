a6b472057b831c5f8c3029d9cb8eb8888cb7f971
Ben Cooper <benjamin.cooper@convoy.com>
2021-01-03 14:29:14 -0600

    remove substitution thingy

--- scripts/verifyNode.sh
+++ scripts/verifyNode.sh
@@ -1,11 +1,11 @@
 # run from root directory
-runningVersion=$(node -v)
-requiredVersion=$(cat ./.nvmrc)
-
 # node -v prefixes version with a v
-if [ "$requiredVersion" = "${runningVersion//v}" ]; then
+runningVersion="$(node -v)"
+requiredVersion="v$(cat ./.nvmrc)"
+
+if [ "$requiredVersion" = "$runningVersion" ]; then
     echo "Node version matches required version."
 else
-    echo "Running Node version ${runningVersion//v}. Must run Node version ${requiredVersion}."
+    echo "Running Node version $runningVersion. Must run Node version $requiredVersion."
     exit 1
 fi
