e0f850b014da2177026d9dcc2c29acd16f41e2c6
Matt Leon <ml@mattleon.com>
2021-01-13 14:14:05 -0500

    Add stories for routes/events/ManageEvents.tsx

---
+++ src/client/routes/events/ManageEvents.stories.tsx
@@ -0,0 +1,62 @@
+import React from 'react';
+import { Meta, Story } from '@storybook/react'; // eslint-disable-line import/no-extraneous-dependencies
+import { MockedProvider, MockedResponse } from '@apollo/client/testing';
+import { MemoryRouter } from 'react-router-dom';
+import {
+	CompaniesDocument,
+	CompaniesQueryResult,
+	EventsDocument,
+	EventsQueryResult,
+} from '../../generated/graphql';
+import Component from './ManageEvents';
+
+export default {
+	title: 'Routes/Events/Manage Events',
+	component: Component,
+} as Meta;
+
+const mocks: MockedResponse[] = [
+	{
+		request: {
+			query: CompaniesDocument,
+		},
+		result: {
+			data: {
+				companies: [
+					{
+						tier: { name: 'platinum', id: '1', permissions: ['nfc'] },
+						id: '1',
+						name: 'Our Favorite Sponsor',
+					},
+				],
+			},
+		} as CompaniesQueryResult,
+	},
+	{
+		request: {
+			query: EventsDocument,
+		},
+		result: {
+			data: {
+				events: [
+					{
+						duration: 30000,
+						eventType: 'checkin',
+						id: '1',
+						name: 'Example Event',
+						startTimestamp: new Date(2021, 1, 1, 1, 1).getTime(),
+						owner: { id: '1', name: 'Our Favorite Sponsor' },
+					},
+				],
+			},
+		} as EventsQueryResult,
+	},
+];
+
+export const ManageEvents: Story = args => (
+	<MockedProvider mocks={mocks}>
+		<MemoryRouter>
+			<Component {...args} />
+		</MemoryRouter>
+	</MockedProvider>
+);
