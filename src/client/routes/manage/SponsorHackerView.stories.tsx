601350f6e50692c5dcc22abbd60b6bc13898a999
Matt Leon <ml@mattleon.com>
2021-02-23 07:38:06 -0500

    Add stories for SponsorHackerView

---
+++ src/client/routes/manage/SponsorHackerView.stories.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { Meta, Story } from '@storybook/react'; // eslint-disable-line import/no-extraneous-dependencies
+import { MemoryRouter } from 'react-router-dom';
+import { SponsorHackerView as Component } from './SponsorHackerView';
+import { MOCK_HACKER } from '../../../common/mockObjects';
+
+export default {
+	title: 'Routes/Manage/Hacker Table/Sponsor Hacker View',
+	component: Component,
+} as Meta;
+
+export const SponsorHackerView: Story = args => <Component {...args} />;
+SponsorHackerView.decorators = [
+	StoryFunc => (
+		<MemoryRouter initialEntries={['/view/hackers']}>
+			<StoryFunc />
+		</MemoryRouter>
+	),
+];
+
+export const SponsorHackerDetailView: Story = args => <Component {...args} />;
+SponsorHackerDetailView.decorators = [
+	StoryFunc => (
+		<MemoryRouter initialEntries={[`/view/hackers/detail/${MOCK_HACKER.id}`]}>
+			<StoryFunc />
+		</MemoryRouter>
+	),
+];
