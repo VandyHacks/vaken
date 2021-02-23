fc340d9485ab443f7d1c9309b7439c39be65ec74
Matt Leon <ml@mattleon.com>
2021-02-23 07:38:13 -0500

    Add stories for NfcTable

--- src/client/routes/nfc/Nfc.stories.tsx
+++ src/client/routes/nfc/Nfc.stories.tsx
@@ -1,29 +1,10 @@
 import React from 'react';
 import { Meta, Story } from '@storybook/react'; // eslint-disable-line import/no-extraneous-dependencies
-import { useImmer } from 'use-immer';
-import { defaultTableState, TableContext } from '../../contexts/TableContext';
 import Component from './Nfc';
-import { MOCK_HACKERS } from '../../../common/mockObjects';
 
 export default {
-	title: 'Routes/NFC/NFC',
+	title: 'Routes/NFC',
 	component: Component,
 } as Meta;
 
-// TableContext works better inside the story than as a story decorator because
-// the latter method forcibly re-renders the entire story when changed.
-const Nfc: Story<Record<string, unknown>> = args => {
-	const [tableState, setTableState] = useImmer(defaultTableState);
-	return (
-		<TableContext.Provider value={{ state: tableState, update: setTableState }}>
-			<Component {...args} />
-		</TableContext.Provider>
-	);
-};
-
-export const WithFakeData: Story<Record<string, unknown>> = args => <Nfc {...args} />;
-WithFakeData.args = {
-	data: MOCK_HACKERS,
-	isSponsor: false,
-	viewResumes: true,
-};
+export const Nfc: Story<Record<string, unknown>> = args => <Component {...args} />;
