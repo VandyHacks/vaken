3217a45676aa451f5656ffcf1a1c3f7cca9440a8
Juyoung Kim <juyoung.kim304@gmail.com>
2021-06-13 13:19:15 -0400

    Made button conditional based on resume permissions, added hackathon start/end date

--- src/common/constants.ts
+++ src/common/constants.ts
@@ -1,2 +1,5 @@
 export const DEADLINE_TIMESTAMP = 1633323540000;
 export const MAX_TEAM_SIZE = 4;
+// TODO: Update with real hackathon start/end dates
+export const HACKATHON_START = new Date('October 8, 2021 00:00:00').getTime();
+export const HACKATHON_END= new Date('October 10, 2021 11:59:00').getTime();
