169ca044e5f39041db853179c39a9d209d2015d3
SamLee514 <samuel.r.lee@vanderbilt.edu>
2021-06-14 22:08:57 -0500

    Pushed to GH instead of heroku, this reverts that
    This reverts commit ca8e055ab11d91d1f889632fe36c5c96bba52ca2.

--- src/server/index.ts
+++ src/server/index.ts
@@ -46,50 +46,7 @@ export const schema = makeExecutableSchema({
 	const dbClient = new DB();
 	const models = await dbClient.collections;
 
-	// app.use(helmet()); // sets good security defaults, see https://helmetjs.github.io/
-
-	app.use(
-		helmet({
-			// {
-			contentSecurityPolicy: false,
-			// useDefaults: true,
-			// 		directives: {
-			// 			...helmet.contentSecurityPolicy.getDefaultDirectives(),
-			// 			'connect-src': [
-			// 				"'self'",
-			// 				'https://storage.googleapis.com/vaken-resume-dump/60846a6cbd2af64a98f39eb9?X-Goog-Algorithm=GOOG4-RSA-SHA256&X-Goog-Credential=resume-manager%40vandyhacks-test.iam.gserviceaccount.com%2F20210608%2Fauto%2Fstorage%2Fgoog4_request&X-Goog-Date=20210608T162810Z&X-Goog-Expires=901&X-Goog-SignedHeaders=content-type%3Bhost&X-Goog-Signature=4bc8fbe2183683ebfc79ed51a817ef14175c569a660c7f21a751882ec3c6d4cdbb110918fc2ac98ce79ffd19e8e9364339a9e669e80eb5080855d8d695559da0cb8ab1c4ce2469e4a8e02de64aa597b4ce2e9b29cbd0ad5bbe6a2bf765cda44753c5862af35c2a5a2c43f1b967b6371b61fb8270e257da06df37ddffd9c548414dc89c1e548d8025aef30db47a441d651df94775cf71c3ab34d1686f3343cbad961626c2dc957b08581205a6b59480677c1f2531fb46f478073f4e8c0b76ba9bd300e4dc6073293a733af1089abeb0cb5e2b0129e3daa0ee2bfe4cd26629ba7c38bd9705f93679626dedf0b10cbb850c37dc4a1316055b00e6231c6040f93eb0',
-			// 				'https://o307001.ingest.sentry.io/api/1234567/store/?sentry_key=00591ab5e07047ff89b475b334837339&sentry_version=7',
-			// 			],
-			// 		},
-			// 	},
-			crossOriginResourcePolicy: { policy: 'cross-origin' },
-		})
-	);
-
-	app.all('*', function (req, res, next) {
-		res.header('Access-Control-Allow-Origin', '*');
-		res.header('Access-Control-Allow-Credentials', 'true');
-		res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
-		res.header(
-			'Access-Control-Allow-Headers',
-			'Origin, X-Requested-With, Content-Type, Accept, Authorization'
-		);
-		next();
-	});
-
-	// Sets all of the defaults, but overrides `script-src` and disables the default `style-src`
-	// app.use(
-	// 	helmet.contentSecurityPolicy({
-	// 		useDefaults: true,
-	// 		directives: {
-	// 			'connect-src': [
-	// 				"'self'",
-	// 				'https://storage.googleapis.com/vaken-resume-dump/60846a6cbd2af64a98f39eb9?X-Goog-Algorithm=GOOG4-RSA-SHA256&X-Goog-Credential=resume-manager%40vandyhacks-test.iam.gserviceaccount.com%2F20210608%2Fauto%2Fstorage%2Fgoog4_request&X-Goog-Date=20210608T162810Z&X-Goog-Expires=901&X-Goog-SignedHeaders=content-type%3Bhost&X-Goog-Signature=4bc8fbe2183683ebfc79ed51a817ef14175c569a660c7f21a751882ec3c6d4cdbb110918fc2ac98ce79ffd19e8e9364339a9e669e80eb5080855d8d695559da0cb8ab1c4ce2469e4a8e02de64aa597b4ce2e9b29cbd0ad5bbe6a2bf765cda44753c5862af35c2a5a2c43f1b967b6371b61fb8270e257da06df37ddffd9c548414dc89c1e548d8025aef30db47a441d651df94775cf71c3ab34d1686f3343cbad961626c2dc957b08581205a6b59480677c1f2531fb46f478073f4e8c0b76ba9bd300e4dc6073293a733af1089abeb0cb5e2b0129e3daa0ee2bfe4cd26629ba7c38bd9705f93679626dedf0b10cbb850c37dc4a1316055b00e6231c6040f93eb0',
-	// 				'https://o307001.ingest.sentry.io/api/1234567/store/?sentry_key=00591ab5e07047ff89b475b334837339&sentry_version=7',
-	// 			],
-	// 		},
-	// 	})
-	// );
+	app.use(helmet()); // sets good security defaults, see https://helmetjs.github.io/
 
 	// Register auth functions
 	app.use(
