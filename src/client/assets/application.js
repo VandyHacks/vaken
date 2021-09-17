e1bfcbcc2c3c37814d80662b97a9504b40fff694
Akash <Ace314159@users.noreply.github.com>
2021-09-17 02:49:50 -0500

    Use autocomplete for state field (#838)
    * Use autocomplete for state field
    
    * Use full state names
    
    * Fix typo

--- src/client/assets/application.js
+++ src/client/assets/application.js
@@ -216,10 +216,12 @@ export const questions = [
 				placeholder: 'Nashville',
 			},
 			{
-				Component: TextInput,
+				Component: AutoComplete,
 				fieldName: 'state',
 				placeholder: 'Tennessee',
+				note: 'If not found, please type the abbreviation.',
 				optional: true,
+				options: import('./data/states.json'),
 				title: 'State',
 			},
 			{
