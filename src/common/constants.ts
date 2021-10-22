cff43f1d17ec51781ab1592327241c1e1ca007fc
Matt Leon <ml@mattleon.com>
2021-10-22 14:10:24 -0400

    Allow sponsors to download resumes (#823)

--- src/common/constants.ts
+++ src/common/constants.ts
@@ -1,2 +1,5 @@
-export const DEADLINE_TIMESTAMP = 1601679600000;
+export const DEADLINE_TIMESTAMP = 1633323540000;
 export const MAX_TEAM_SIZE = 4;
+
+export const HACKATHON_START = new Date('October 8, 2021 17:00:00').getTime();
+export const HACKATHON_END = new Date('October 10, 2021 15:00:00').getTime();
--- src/server/storage/gcp.ts
+++ src/server/storage/gcp.ts
@@ -1,4 +1,7 @@
 import { Storage, GetSignedUrlConfig } from '@google-cloud/storage';
+import JSZip from 'jszip';
+import DB from '../models';
+import { RESUME_DUMP_NAME } from '../../client/assets/strings.json';
 
 const { BUCKET_NAME, GCP_STORAGE_SERVICE_ACCOUNT } = process.env;
 
@@ -13,6 +16,9 @@ if (GCP_STORAGE_SERVICE_ACCOUNT == null) {
 export const getSignedUploadUrl = async (filename: string): Promise<string> => {
 	const credentials = JSON.parse(GCP_STORAGE_SERVICE_ACCOUNT);
 	const storage = new Storage({ credentials });
+
+	// Check for resume dump. Remove if exists.
+	await storage.bucket(BUCKET_NAME).file(RESUME_DUMP_NAME).delete({ ignoreNotFound: true });
 
 	const options: GetSignedUrlConfig = {
 		action: 'write' as const,
@@ -42,4 +48,31 @@ export const getSignedReadUrl = async (filename: string): Promise<string> => {
 	const [url] = await storage.bucket(BUCKET_NAME).file(filename).getSignedUrl(options);
 
 	return url;
+};
+
+export const getResumeDumpUrl = async (): Promise<string> => {
+	const credentials = JSON.parse(GCP_STORAGE_SERVICE_ACCOUNT);
+	const bucket = new Storage({ credentials }).bucket(BUCKET_NAME);
+	const models = await new DB().collections;
+	if (!(await bucket.file(RESUME_DUMP_NAME).exists())[0]) {
+		const zip = JSZip();
+
+		await Promise.all(
+			await models.Hackers.find({
+				status: { $in: ['ACCEPTED', 'SUBMITTED', 'CONFIRMED'] },
+			})
+				.map(async hacker => {
+					const storedFilename = hacker._id.toHexString();
+					const fileContents = (await bucket.file(storedFilename).download())[0];
+					const readableFilename = `${hacker.lastName}, ${hacker.firstName} (${hacker.school}).pdf`;
+					zip.file(readableFilename, fileContents);
+				})
+				.toArray()
+		);
+
+		const dump = await zip.generateAsync({ type: 'nodebuffer' });
+		await bucket.file(RESUME_DUMP_NAME).save(dump, { resumable: false });
+	}
+
+	return getSignedReadUrl(RESUME_DUMP_NAME);
 };
